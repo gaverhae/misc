d627eb5513712b90c568295cda7207c7f727099c
Gary Verhaegen <gary.verhaegen@gmail.com>
2022-05-18 14:59:29 +0200

    tweaks

--- collect_data.sh
+++ collect_data.sh
@@ -7,20 +7,19 @@ format () {
         echo "[$(date -Is)] $1: $line"
     done
 }
-
 exec 1> >(format out >&1)
 exec 2> >(format err >&2)
 
 BUS=(/Volumes/.timemachine/*/*/*.backup)
+tmp=$(mktemp)
 
 for i in $(seq 1 $((${#BUS[@]} - 2))); do
-    echo "Comparing $(basename ${BUS[$i]}) <> $(basename ${BUS[$((i+1))]})..."
     target_file=cumul/$(basename ${BUS[$i]})_$(basename ${BUS[i+1]}).txt
     if [ -f "$target_file" ]; then
-        echo "$target_file exists"
+        echo "$target_file exists; skipping"
     else
-        echo "$target_file does not exist"
+        echo "Comparing $(basename ${BUS[$i]}) <> $(basename ${BUS[i+1]})..."
+        tmutil compare "${BUS[$i]}" "${BUS[i+1]}" > $tmp
+        mv $tmp $target_file
     fi
-    #echo $target_file
-    #tmutil compare "${BUS[$i]}" "${BUS[i+1]}" > $(basename ${BUS[$i]})_$(basename ${BUS[i+1]}).txt
 done
