15583ed03685b0bbba6a5e4942c7965fbb274336
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-04-24 13:39:41 +0200

    c/wpe: wire things

---
+++ courses/2024/weekly-python-exercise-2022-b3/day01/solution/main.py
@@ -0,0 +1,23 @@
+class Serializable:
+    def dump(self, path):
+        pass
+    def load(self, path):
+        pass
+
+class JSONMixin:
+    def dump(self, path):
+        pass
+    def load(self, path):
+        pass
+
+class XMLMixin:
+    def dump(self, path):
+        pass
+    def load(self, path):
+        pass
+
+class CSVMixin:
+    def dump(self, path):
+        pass
+    def load(self, path):
+        pass
--- courses/2024/weekly-python-exercise-2022-b3/shell.nix
+++ courses/2024/weekly-python-exercise-2022-b3/shell.nix
@@ -6,6 +6,7 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     bash
     curl
+    fswatch
     jq
     poetry
   ];
