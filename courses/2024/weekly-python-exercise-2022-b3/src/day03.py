e9bc4a49191b71a56cb9085bf4c4a7a8b5e30a51
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-05-12 16:38:52 +0200

    looking at solution

--- courses/2024/weekly-python-exercise-2022-b3/src/day03.py
+++ courses/2024/weekly-python-exercise-2022-b3/src/day03.py
@@ -3,15 +3,13 @@ import tarfile
 import tempfile
 import zipfile
 
-def tar_to_zip(*files, **kwargs):
-    out = kwargs.get('zippath', '.')
-    print(out)
+def tar_to_zip(*files, zippath = '.'):
     for file in files:
         if tarfile.is_tarfile(file):
             with tempfile.TemporaryDirectory() as extract_dir:
                 with tarfile.open(file) as tf:
                     tf.extractall(extract_dir)
-                with zipfile.ZipFile(out / file.with_suffix(".zip"), 'x') as zf:
+                with zipfile.ZipFile(pathlib.Path(zippath) / file.with_suffix(".zip"), 'x') as zf:
                     for extracted_file in pathlib.Path(extract_dir).glob('*'):
                         zf.write(extract_dir / extracted_file)
         else:
