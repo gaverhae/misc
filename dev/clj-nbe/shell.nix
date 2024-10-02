b1baaecaade73830c0ec1733e4195c5962342e74
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-10-02 16:02:50 +0200

    add leiningen

--- dev/clj-nbe/shell.nix
+++ dev/clj-nbe/shell.nix
@@ -1,5 +1,6 @@
 let
   pkgs = import ./nix/nixpkgs.nix;
+  jdk = pkgs.openjdk17_headless;
 in
 pkgs.mkShell {
   LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
@@ -7,5 +8,6 @@ pkgs.mkShell {
     bash
     curl
     jq
+    (leiningen.override { jdk = jdk; })
   ];
 }
