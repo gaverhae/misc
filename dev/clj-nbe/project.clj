f4f0035afc46989ba92cf2502f0601e10bbf69b4
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-10-10 19:19:49 +0200

    add example code (#10)

--- dev/clj-nbe/project.clj
+++ dev/clj-nbe/project.clj
@@ -1,3 +1,5 @@
 (defproject t "alpha"
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[hiccup "2.0.0-RC3"]
+                 [org.clojure/clojure "1.12.0"]
+                 [org.clojure/core.match "1.1.0"]]
   :repl-options {:init-ns t.core})
