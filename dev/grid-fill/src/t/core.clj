28f082e2a1daeae8b6fcdb63dada5fadf8fddc75
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-10-13 18:18:47 +0200

    grid-fill: step2: 150ms v. 6ms (#15)

--- dev/grid-fill/src/t/core.clj
+++ dev/grid-fill/src/t/core.clj
@@ -6,47 +6,64 @@
   [& args]
   (println "Hello, World!"))
 
+(defn prevt
+  []
+  (for [x11 (range 1 7) x12 (range 1 7) x13 (range 1 7) x14 (range 1 7) x15 (range 1 7) x16 (range 1 7)
+        :when (< x11 x12)
+        :when (< x12 x13)
+        :when (> x14 x15)
+        :when (< x15 x16)
+        :when (= #{1 2 3 4 5 6} (into #{} [x11 x12 x13 x14 x15 x16]))
+        x21 (range 1 7) x22 (range 1 7) x23 (range 1 7) x24 (range 1 7) x25 (range 1 7) x26 (range 1 7)
+        :when (> x21 x22)
+        :when (> x24 x14)
+        :when (> x24 x25)
+        :when (> x25 x26)
+        :when (= #{1 2 3 4 5 6} (into #{} [x21 x22 x23 x24 x25 x26]))
+        :when (and (not= x11 x21)
+                   (not= x12 x22)
+                   (not= x13 x23)
+                   (not= x14 x24)
+                   (not= x15 x25)
+                   (not= x16 x26))]
+    [[x11 x12 x13 x14 x15 x16]
+     [x21 x22 x23 x24 x25 x26]]))
+
 (def r (set (range 1 7)))
+
+(defn t
+  []
+  (for [x11 (range 1 5)
+        x12 (range (inc x11) 6)
+        x13 (range (inc x12) 7)
+        x14 (range 2 6)
+        :when (and (not= x11 x14) (not= x12 x14) (not= x13 x14))
+        x15 (range 1 x14)
+        :when (and (not= x15 x11) (not= x15 x12) (not= x15 x13))
+        x16 (range (inc x15) 7)
+        :when (and (not= x16 x11) (not= x16 x12) (not= x16 x13) (not= x16 x14))
+        x21 (range 2 7)
+        :when (not= x21 x11)
+        x22 (range 1 x21)
+        :when (not= x22 x12)
+        x23 (range 1 7)
+        :when (and (not= x23 x21) (not= x23 x22) (not= x23 x13))
+        x24 (range (inc x14) 7)
+        :when (and (not= x24 x21) (not= x24 x22) (not= x24 x23) (not= x24 x14))
+        x25 (range 2 x24)
+        :when (and (not= x25 x21) (not= x25 x22) (not= x25 x23) (not= x25 x15))
+        x26 (range 1 x25)
+        :when (and (not= x26 x21) (not= x26 x22) (not= x26 x23) (not= x26 x16))]
+    [[x11 x12 x13 x14 x15 x16]
+     [x21 x22 x23 x24 x25 x26]]))
 
 (comment
 
-  (defn prevt
-    []
-    (for [x11 (range 1 7) x12 (range 1 7) x13 (range 1 7) x14 (range 1 7) x15 (range 1 7) x16 (range 1 7)
-          :when (< x11 x12)
-          :when (< x12 x13)
-          :when (> x14 x15)
-          :when (< x15 x16)
-          :when (= #{1 2 3 4 5 6} (into #{} [x11 x12 x13 x14 x15 x16]))]
-      [x11 x12 x13 x14 x15 x16]))
+  (time (count (prevt)))
+  (time (count (t)))
 
 
 
-  (defn t
-    []
-    (for [x11 r
-          x12 (range (inc x11) 7)
-          x13 (range (inc x12) 7)
-          x14 (range 1 7)
-          :when (and (not= x11 x14) (not= x12 x14) (not= x13 x14))
-          x15 (range 1 x14)
-          :when (and (not= x15 x11) (not= x15 x12) (not= x15 x13))
-          x16 (range (inc x15) 7)
-          :when (and (not= x16 x11) (not= x16 x12) (not= x16 x13) (not= x16 x14))
-          :when (= 21 (+ x11 x12 x13 x14 x15 x16))
-          :when (= r (into #{} [x11 x12 x13 x14 x15 x16]))]
-      [x11 x12 x13 x14 x15 x16]))
-          x21 (range 1 7)
-          x22 (range 1 7)
-          x23 (range 1 7)
-          x24 (range 1 7)
-          x25 (range 1 7)
-          x26 (range 1 7)
-          :when (> x21 x22)
-          :when (> x24 x14)
-          :when (> x24 x25)
-          :when (> x25 x26)
-          :when (= #{1 2 3 4 5 6} (into #{} [x21 x22 x23 x24 x25 x26]))
           x31 (range 1 7) x32 (range 1 7) x33 (range 1 7) x34 (range 1 7) x35 (range 1 7) x36 (range 1 7)
           :when (< x31 x32)
           :when (< x33 x34)
@@ -73,13 +90,13 @@
           :when (= #{1 2 3 4 5 6} (into #{} [x13 x23 x33 x43 x53 x63]))
           :when (= #{1 2 3 4 5 6} (into #{} [x14 x24 x34 x44 x54 x64]))
           :when (= #{1 2 3 4 5 6} (into #{} [x15 x25 x35 x45 x55 x65]))
-          :when (= #{1 2 3 4 5 6} (into #{} [x16 x26 x36 x46 x56 x66]))]
+          :when (= #{1 2 3 4 5 6} (into #{} [x16 x26 x36 x46 x56 x66]))
       [[x11 x12 x13 x14 x15 x16]
        [x21 x22 x23 x24 x25 x26]
        [x31 x32 x33 x34 x35 x36]
        [x41 x42 x43 x44 x45 x46]
        [x51 x52 x53 x54 x55 x56]
-       [x61 x62 x63 x64 x65 x66]]))
+       [x61 x62 x63 x64 x65 x66]]
 
 
 )
