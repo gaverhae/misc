56fd520d5ffb7a2b99b00c562189798597b2ad9e
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-11-04 12:52:37 +0100

    monthly dependencies update

--- dev/grid-fill/project.clj
+++ dev/grid-fill/project.clj
@@ -1,5 +1,5 @@
 (defproject t "app"
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.12.0"]]
   :main ^:skip-aot t.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
