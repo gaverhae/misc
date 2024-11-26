59416464a598431d011ee576ec4e6ad7eea0fa7e
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-11-26 16:02:36 +0100

    books/2015/pawga/ch3/my: victory conditions

--- books/2015/pro-android-web-game-apps-2012/public/ch3/my/Game.js
+++ books/2015/pro-android-web-game-apps-2012/public/ch3/my/Game.js
@@ -10,8 +10,20 @@ var Game = function() {
       var col = GameController.move_from_click(dims, x, y);
       if (col != -1) {
         model = GameModel.move(model, col);
-        GameRenderer.render(canvas.getContext("2d"), dims, model);
       }
+      switch (model.status[0]) {
+        case "win":
+          alert("Player " + model.current_player + " wins!");
+          model = GameModel.init(6, 7);
+          break;
+        case "draw":
+          alert("It's a draw!");
+          model = GameModel.init(6, 7);
+          break;
+        case "ongoing":
+          break;
+      }
+      GameRenderer.render(canvas.getContext("2d"), dims, model);
     }
     canvas.addEventListener("click", function(e) {
       handle_click(e.x, e.y);
