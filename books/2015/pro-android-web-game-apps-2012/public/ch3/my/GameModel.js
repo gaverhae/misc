5d5ba1752ab36174fa6529c5849749ba94207eb2
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-11-26 13:48:41 +0100

    books/2015/pawga/ch3/my: ability to play a turn

--- books/2015/pro-android-web-game-apps-2012/public/ch3/my/GameModel.js
+++ books/2015/pro-android-web-game-apps-2012/public/ch3/my/GameModel.js
@@ -3,13 +3,22 @@ var GameModel = function() {
     return {
       rows: rows,
       cols: cols,
-      tokens: [],
+      tokens: Array.apply(null, Array(cols)).map(() => []),
       player_color: ["red", "green"],
       current_player: 0,
       state: ["ongoing"]
     };
   }
   function move(state, column) {
+    if (column < 0 || column >= state.cols) {
+      return ["failure", "invalid column"];
+    }
+    if (state.tokens[column].length == state.cols) {
+      return ["failure", "column full"];
+    }
+    state.tokens[column].push(state.current_player);
+    state.current_player = 1 - state.current_player;
+    return ["success", state];
   }
   return {
     init: init,
