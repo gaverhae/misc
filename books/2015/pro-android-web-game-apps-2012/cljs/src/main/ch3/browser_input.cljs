ee7165d86f6520b6b11040480717ef3602908916
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-11-29 18:50:24 +0100

    books/2015/pawga/ch3/cljs: handle resize event (#87)

--- books/2015/pro-android-web-game-apps-2012/cljs/src/main/ch3/browser_input.cljs
+++ books/2015/pro-android-web-game-apps-2012/cljs/src/main/ch3/browser_input.cljs
@@ -1,13 +1,14 @@
 (ns ch3.browser-input
   (:require [clojure.core.async :as async]))
 
+(defn send-resize-event
+  [>bus]
+  (async/put! >bus [:resize js/document.body.clientWidth js/document.body.clientHeight]))
+
 (defn start-input-loop!
   [>bus canvas-id]
-  (js/window.addEventListener
-    "resize"
-    (fn [_]
-      (prn [:resize])
-      (async/put! >bus [:resize])))
+  (js/window.addEventListener "resize" #(send-resize-event >bus))
+  (send-resize-event >bus)
   (.addEventListener
     (js/document.getElementById canvas-id)
     "click"
