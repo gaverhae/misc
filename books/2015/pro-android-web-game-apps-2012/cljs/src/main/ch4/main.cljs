1c468485828f4401c0a2ef5ee9f36e74dd0b3871
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-04 13:56:00 +0100

    books/2015/pawga/ch4: simple image loading (#106)

--- books/2015/pro-android-web-game-apps-2012/cljs/src/main/ch4/main.cljs
+++ books/2015/pro-android-web-game-apps-2012/cljs/src/main/ch4/main.cljs
@@ -1,5 +1,43 @@
-(ns ch4.main)
+(ns ch4.main
+  (:require [clojure.core.async :as async]
+            [clojure.core.match :refer [match]]))
+
+(def knight-sheet
+  {:path "/img/spritesheet.png"
+   :sprites (->> [[0, 0, 94, 76, 44, 67],
+                  [94, 0, 94, 76, 44, 68],
+                  [188, 0, 91, 76, 42, 68],
+                  [279, 0, 88, 76, 39, 68],
+                  [367, 0, 83, 77, 34, 69],
+                  [0, 77, 81, 75, 34, 67],
+                  [81, 77, 80, 73, 34, 66],
+                  [161, 77, 78, 72, 33, 65],
+                  [239, 77, 77, 72, 33, 65],
+                  [316, 77, 77, 72, 32, 65],
+                  [393, 77, 79, 73, 33, 66],
+                  [0, 152, 80, 75, 34, 67],
+                  [80, 152, 81, 77, 34, 69],
+                  [161, 152, 84, 77, 36, 69],
+                  [245, 152, 89, 76, 41, 68],
+                  [334, 152, 93, 75, 44, 67]]
+                 (map (fn [[tx ty w h ax ay]]
+                        {:rectangle [tx ty w h]
+                         :anchor [ax ay]})))})
+
+(defn start-image-loader!
+  [images]
+  (let [evt-src (async/chan)]
+    (doseq [img images]
+      (let [i (js/Image.)]
+        (set! (.-onload i) (fn [] (async/put! evt-src [:loaded (assoc img :image i)])))
+        (set! (.-src i) (:path img))))
+    (async/go
+      (loop []
+        (when-let [msg (async/<! evt-src)]
+          (js/console.log (pr-str msg))
+          (js/console.log (:image (second msg))))))))
 
 (defn init
   []
+  (start-image-loader! [knight-sheet])
   (js/console.log "hello"))
