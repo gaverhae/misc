3f4ac0eff02e40135b649abf04b0d4e9ffa1fb36
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-05 21:08:16 +0100

    aoc/2024/03: part 2

--- advent_of_code/2024/src/t/day03.clj
+++ advent_of_code/2024/src/t/day03.clj
@@ -1,31 +1,40 @@
 (ns t.day03
-  (:require [clojure.string :as string]
+  (:require [clojure.core.match :refer [match]]
+            [clojure.string :as string]
             [t.lib :as lib :refer [->long]]))
 
 (defn parse
   [lines]
   (->> lines
        (mapcat (fn [l]
-                 (re-seq #"mul\(\d{1,3},\d{1,3}\)" l)))))
-
-(defn solve
-  [input]
-  (->> input
-       (map (fn [m]
-              (let [[_ a b] (re-matches #"mul\((\d+),(\d+)\)" m)]
-                (* (->long a) (->long b)))))
-       (reduce + 0)))
+                 (re-seq #"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)" l)))
+       (map (fn [s]
+              (case (subs s 0 3)
+                "mul" (let [[_ a b] (re-matches #"mul\((\d+),(\d+)\)" s)]
+                        (* (->long a) (->long b)))
+                "don" :off
+                "do(" :on)))))
 
 (defn part1
   [input]
-  (solve input))
+  (->> input
+       (remove #{:on :off})
+       (reduce + 0)))
 
 (defn part2
   [input]
-  (solve input))
+  (->> input
+       (reduce (fn [[on? tot] el]
+                 (match [on? el]
+                   [_ :on] [true tot]
+                   [_ :off] [false tot]
+                   [true n] [true (+ tot n)]
+                   [false _] [false tot]))
+               [true 0])
+       second))
 
 (lib/check
   [part1 sample] 161
   [part1 puzzle] 156388521
-  #_#_[part2 sample] 0
-  #_#_[part2 puzzle] 0)
+  [part2 sample1] 48
+  [part2 puzzle] 75920122)
