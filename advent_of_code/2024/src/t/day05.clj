62be10b0676c1ec8fe4387705cc840c30be6221c
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-05 21:54:07 +0100

    aoc/2024/05: part 1

--- advent_of_code/2024/src/t/day05.clj
+++ advent_of_code/2024/src/t/day05.clj
@@ -1,26 +1,48 @@
 (ns t.day05
   (:require [clojure.core.match :refer [match]]
+            [clojure.set :as set]
             [clojure.string :as string]
             [t.lib :as lib :refer [->long]]))
 
 (defn parse
   [lines]
-  lines)
+  (let [rules (->> lines
+                   (take-while #(not= "" %))
+                   (map (fn [r] (string/split r #"\|")))
+                   (reduce (fn [acc [bef af]]
+                             (update acc bef (fnil conj #{}) af))
+                           {}))
+        updates (->> lines
+                     (drop-while #(not= "" %))
+                     rest
+                     (map (fn [line]
+                            (vec (string/split line #",")))))]
 
-(defn solve
-  [lines]
-  lines)
+    [rules updates]))
+
+(defn is-safe?
+  [rules]
+  (fn [u]
+    (->> (range (count u))
+         (map (fn [c] [(take c u) (get u c)]))
+         (every? (fn [[befs p]]
+                   (empty? (set/intersection (set befs)
+                                             (rules p))))))))
 
 (defn part1
-  [input]
-  (solve input))
+  [[rules updates]]
+  (->> updates
+       (filter (is-safe? rules))
+       (map (fn [u] (get u (quot (count u) 2))))
+       (map ->long)
+       (reduce + 0)))
 
 (defn part2
   [input]
-  (solve input))
+  input)
 
 (lib/check
-  [part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample] 143
+  [part1 puzzle] 5108
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
