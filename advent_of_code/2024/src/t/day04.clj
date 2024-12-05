d3f1d3f2491aec91e3ca383bbd640a0f1d609c3b
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-05 21:32:32 +0100

    aoc/2024/04: part 2

--- advent_of_code/2024/src/t/day04.clj
+++ advent_of_code/2024/src/t/day04.clj
@@ -5,37 +5,48 @@
 
 (defn parse
   [lines]
-  (let [t (->> lines
-               (map vec)
-               vec)
-        X [\X \M \A \S]]
-    (for [tpl [[[0 0] [0 1] [0 2] [0 3]]
-               [[0 0] [1 0] [2 0] [3 0]]
-               [[0 0] [1 1] [2 2] [3 3]]
-               [[0 0] [1 -1] [2 -2] [3 -3]]]
-          line (range (count t))
-          col (range (count (t line)))
-          :let [c (->> tpl
-                       (map (fn [[x y]]
-                              [(+ x col) (+ y line)]))
-                       (map (fn [p] (get-in t p))))]
-          :when (or (= c X) (= (reverse c) X))]
-      1)))
-
-(defn solve
-  [lines]
-  (count lines))
+  (->> lines
+       (map vec)
+       vec))
 
 (defn part1
-  [input]
-  (solve input))
+  [t]
+  (let [X [\X \M \A \S]]
+    (->> (for [tpl [[[0 0] [0 1] [0 2] [0 3]]
+                    [[0 0] [1 0] [2 0] [3 0]]
+                    [[0 0] [1 1] [2 2] [3 3]]
+                    [[0 0] [1 -1] [2 -2] [3 -3]]]
+               line (range (count t))
+               col (range (count (t line)))
+               :let [c (->> tpl
+                            (map (fn [[x y]]
+                                   [(+ x col) (+ y line)]))
+                            (map (fn [p] (get-in t p))))]
+               :when (or (= c X) (= (reverse c) X))]
+           1)
+         count)))
 
 (defn part2
-  [input]
-  (solve input))
+  [t]
+  (let [tpl [[0 0]       [0 2]
+                   [1 1]
+             [2 0]       [2 2]]
+        X [\M \A \S]]
+    (->> (for [line (range (count t))
+               col (range (count (t line)))
+               :let [c (->> tpl
+                            (map (fn [[x y]]
+                                   [(+ x col) (+ y line)]))
+                            (mapv (fn [p] (get-in t p))))
+                     b1 [(c 0) (c 2) (c 4)]
+                     b2 [(c 1) (c 2) (c 3)]]
+               :when (and (or (= b1 X) (= (reverse b1) X))
+                          (or (= b2 X) (= (reverse b2) X)))]
+           1)
+         count)))
 
 (lib/check
   [part1 sample] 18
   [part1 puzzle] 2468
-  #_#_[part2 sample] 0
-  #_#_[part2 puzzle] 0)
+  [part2 sample] 9
+  [part2 puzzle] 1864)
