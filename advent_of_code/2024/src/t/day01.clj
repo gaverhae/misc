a5c303bbb739c0d4c36dd1e81b004f11e82d2515
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-05 20:16:34 +0100

    aoc/2024/01: prep (#169)

---
+++ advent_of_code/2024/src/t/day01.clj
@@ -0,0 +1,24 @@
+(ns t.day01
+  (:require [t.lib :as lib :refer [->long]]))
+
+(defn parse
+  [lines]
+  lines)
+
+(defn solve
+  [lines]
+  0)
+
+(defn part1
+  [input]
+  (solve input))
+
+(defn part2
+  [input]
+  (solve input))
+
+(lib/check
+  [part1 sample] 1
+  [part1 puzzle] 1
+  [part2 sample] 1
+  [part2 puzzle] 1)
