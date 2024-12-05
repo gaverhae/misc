1087910c873889bdcf2e1bf95a2518f62e8e0713
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-05 20:45:23 +0100

    aoc/2024/02: part 2

--- advent_of_code/2024/src/t/day02.clj
+++ advent_of_code/2024/src/t/day02.clj
@@ -8,27 +8,29 @@
        (map (fn [s] (->> (string/split s #" ")
                          (map ->long))))))
 
-(defn solve
-  [lines]
-  (->> lines
-       (filter (fn [r]
-                 (and (or (= r (sort r))
-                          (= r (reverse (sort r))))
-                      (->> r
-                           (partition 2 1)
-                           (every? (fn [[a b]] (<= 1 (abs (- a b)) 3)))))))
-       count))
+(defn safe?
+  [r]
+  (and (or (= r (sort r))
+           (= r (reverse (sort r))))
+       (->> r
+            (partition 2 1)
+            (every? (fn [[a b]] (<= 1 (abs (- a b)) 3))))))
 
 (defn part1
   [input]
-  (solve input))
+  (->> input
+       (filter safe?)
+       count))
 
 (defn part2
   [input]
-  (solve input))
+  (->> input
+       (filter (fn [r] (some safe? (for [p (range (count r))]
+                                  (concat (take p r) (drop (inc p) r))))))
+       count))
 
 (lib/check
   [part1 sample] 2
   [part1 puzzle] 564
-  #_#_[part2 sample] 0
-  #_#_[part2 puzzle] 0)
+  [part2 sample] 4
+  [part2 puzzle] 604)
