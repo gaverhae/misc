0fb835dd34c5ec33ceeccce6e8a1b6a5ac71d149
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-06 13:53:05 +0100

    aoc/24: init-nix (#148)

---
+++ advent_of_code/2024/shell.nix
@@ -0,0 +1,13 @@
+let
+  pkgs = import ./nix/nixpkgs.nix;
+  getFlake = url: (builtins.getFlake url).packages.${pkgs.system}.default;
+in
+pkgs.mkShell {
+  SSL_CERT_FILE = "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt";
+  LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+  buildInputs = with pkgs; [
+    bash
+    curl
+    jq
+  ];
+}
