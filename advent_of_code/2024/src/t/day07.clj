378022d07a17710248883136c43f2be5c2f9a3d3
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-07 09:00:55 +0100

    aoc/2024/07: part 1

--- advent_of_code/2024/src/t/day07.clj
+++ advent_of_code/2024/src/t/day07.clj
@@ -17,16 +17,31 @@
        (map (fn [l] (->> l (map (fn [[_ s]] (->long s))))))
        (map (fn [[tot & ts]] [tot ts]))))
 
+(defn can-work?
+  [[tot [t & ts]]]
+  (loop [possible-values #{t}
+         remaining-terms ts]
+    (if (empty? remaining-terms)
+      (contains? possible-values tot)
+      (let [[t & ts] remaining-terms]
+        (recur (->> possible-values
+                    (mapcat (fn [p] [(+ p t) (* p t)]))
+                    set)
+               ts)))))
+
 (defn part1
   [input]
-  input)
+  (->> input
+       (filter can-work?)
+       (map first)
+       (reduce + 0)))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample] 3749
+  [part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
