44663ac88e2d10ea182efda02179daf933f8f8d2
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-08 07:33:47 +0100

    aoc/2024/08: part 1

--- advent_of_code/2024/src/t/day08.clj
+++ advent_of_code/2024/src/t/day08.clj
@@ -1,22 +1,48 @@
 (ns t.day08
   (:require [clojure.core.match :refer [match]]
+            [clojure.set :as set]
             [clojure.string :as string]
             [t.lib :as lib :refer [->long]]))
 
 (defn parse
   [lines]
-  lines)
+  {:antennas (->> (for [y (range (count lines))
+                        :let [line (get lines y)]
+                        x (range (count line))
+                        :let [c (get line x)]
+                        :when (not= c \.)]
+                    {c #{[y x]}})
+                  (apply merge-with set/union {}))
+   :max-x (dec (count (first lines)))
+   :max-y (dec (count lines))})
+
+(defn antinode
+  [[y1 x1] [y2 x2]]
+  (let [dy (- y2 y1)
+        dx (- x2 x1)]
+    [(+ y2 dy) (+ x2 dx)]))
 
 (defn part1
-  [input]
-  input)
+  [{:keys [antennas max-y max-x]}]
+  (->> antennas
+       (mapcat (fn [[k vs]]
+                 (for [v1 vs
+                       v2 vs
+                       :when (not= v1 v2)
+                       :let [[y x] (antinode v1 v2)]
+                       :when (and (<= 0 y max-y)
+                                  (<= 0 x max-x))]
+                   [y x])))
+       set
+       count))
+
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample] 14
+  [part1 puzzle] 354
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
