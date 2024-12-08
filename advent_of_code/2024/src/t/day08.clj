26c1d9304fc218da52491d2b93cf644788c0f2d2
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-08 07:52:13 +0100

    aoc/2024/08: refactor a bit

--- advent_of_code/2024/src/t/day08.clj
+++ advent_of_code/2024/src/t/day08.clj
@@ -6,50 +6,47 @@
 
 (defn parse
   [lines]
-  {:antennas (->> (for [y (range (count lines))
-                        :let [line (get lines y)]
-                        x (range (count line))
-                        :let [c (get line x)]
-                        :when (not= c \.)]
-                    {c #{[y x]}})
-                  (apply merge-with set/union {}))
-   :max-x (dec (count (first lines)))
-   :max-y (dec (count lines))})
+  {:freq-to-ant (->> (for [y (range (count lines))
+                           :let [line (get lines y)]
+                           x (range (count line))
+                           :let [c (get line x)]
+                           :when (not= c \.)]
+                       {c #{[y x]}})
+                     (apply merge-with set/union {}))
+   :in-bounds? (let [max-y (dec (count lines))
+                     max-x (dec (count (first lines)))]
+                 (fn [[y x]]
+                   (and (<= 0 y max-y)
+                        (<= 0 x max-x))))})
 
-(defn antinode
+(defn step
   [[y1 x1] [y2 x2]]
   (let [dy (- y2 y1)
         dx (- x2 x1)]
-    [(+ y2 dy) (+ x2 dx)]))
+    (fn [[y x]] [(+ y dy) (+ x dx)])))
 
 (defn part1
-  [{:keys [antennas max-y max-x]}]
-  (->> antennas
-       (mapcat (fn [[k vs]]
-                 (for [v1 vs
-                       v2 vs
-                       :when (not= v1 v2)
-                       :let [[y x] (antinode v1 v2)]
-                       :when (and (<= 0 y max-y)
-                                  (<= 0 x max-x))]
-                   [y x])))
+  [{:keys [freq-to-ant in-bounds?]}]
+  (->> freq-to-ant
+       (mapcat (fn [[_frequency antennas]]
+                 (for [a1 antennas
+                       a2 antennas
+                       :when (not= a1 a2)
+                       :let [antinode ((step a1 a2) a2)]
+                       :when (in-bounds? antinode)]
+                   antinode)))
        set
        count))
 
-
 (defn part2
-  [{:keys [antennas max-y max-x]}]
-  (->> antennas
-       (mapcat (fn [[k vs]]
-                 (for [[y1 x1] vs
-                       [y2 x2] vs
-                       :when (not= [y1 x1] [y2 x2])
-                       :let [dy (- y2 y1)
-                             dx (- x2 x1)]]
-                   (->> (iterate (fn [[y x]] [(+ y dy) (+ x dx)]) [y1 x1])
-                        (take-while (fn [[y x]]
-                                      (and (<= 0 y max-y)
-                                           (<= 0 x max-x))))))))
+  [{:keys [freq-to-ant in-bounds?]}]
+  (->> freq-to-ant
+       (mapcat (fn [[_frequency antennas]]
+                 (for [a1 antennas
+                       a2 antennas
+                       :when (not= a1 a2)]
+                   (->> (iterate (step a1 a2) a1)
+                        (take-while in-bounds?)))))
        (apply concat)
        set
        count))
@@ -58,4 +55,4 @@
   [part1 sample] 14
   [part1 puzzle] 354
   [part2 sample] 34
-  [part2 puzzle] 0)
+  [part2 puzzle] 1263)
