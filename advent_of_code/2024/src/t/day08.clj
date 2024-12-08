8978a42425dd734ec30b0dabe7b46525d1dac4da
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-08 07:14:48 +0100

    aoc/2024/08: prep

---
+++ advent_of_code/2024/src/t/day08.clj
@@ -0,0 +1,22 @@
+(ns t.day08
+  (:require [clojure.core.match :refer [match]]
+            [clojure.string :as string]
+            [t.lib :as lib :refer [->long]]))
+
+(defn parse
+  [lines]
+  lines)
+
+(defn part1
+  [input]
+  input)
+
+(defn part2
+  [input]
+  input)
+
+(lib/check
+  [part1 sample] 0
+  #_#_[part1 puzzle] 0
+  #_#_[part2 sample] 0
+  #_#_[part2 puzzle] 0)
