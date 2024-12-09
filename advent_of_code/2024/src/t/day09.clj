7bb86bcc36683d255453e55cd22604645862c4fc
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-09 13:12:20 +0100

    aoc/2024/09: part1 sample, but stackoverflow on puzzle

--- advent_of_code/2024/src/t/day09.clj
+++ advent_of_code/2024/src/t/day09.clj
@@ -7,18 +7,51 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> lines
+       first
+       (reduce (fn [[blank? d idx] c]
+                 (let [n (->long (str c))]
+                   [(not blank?)
+                    (concat d (repeat n (if blank? nil idx)))
+                    (or (and blank? idx)
+                        (inc idx))]))
+               [false [] 0])
+       second))
+
+(defn trim
+  [v]
+  (let [v (vec v)
+        idx (dec (count v))]
+    (loop [idx idx]
+      (if (v idx)
+        (take (inc idx) v)
+        (recur (dec idx))))))
+
+(defn compact
+  [disk]
+  (loop [to-process (trim disk)
+         processed []]
+    (cond (empty? to-process)
+          processed
+          (first to-process)
+          (recur (rest to-process)
+                 (conj processed (first to-process)))
+          :else
+          (recur (->> to-process rest butlast trim)
+                 (conj processed (last to-process))))))
 
 (defn part1
   [input]
-  input)
+  (->> (compact input)
+       (map-indexed *)
+       (reduce + 0)))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample] 1928
+  [part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
