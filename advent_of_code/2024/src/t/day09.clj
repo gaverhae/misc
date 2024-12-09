80050692aa69ba16a57db447fcee0fc9bc23efd1
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-09 13:46:59 +0100

    aoc/2024/09: part2 wrong, misunderstood

--- advent_of_code/2024/src/t/day09.clj
+++ advent_of_code/2024/src/t/day09.clj
@@ -12,7 +12,7 @@
        (reduce (fn [[blank? d idx] c]
                  (let [n (->long (str c))]
                    [(not blank?)
-                    (vec (concat d (repeat n (if blank? nil idx))))
+                    (conj d [(if blank? nil idx) n])
                     (or (and blank? idx)
                         (inc idx))]))
                [false [] 0])
@@ -45,16 +45,45 @@
 
 (defn part1
   [input]
-  (->> (compact input)
+  (->> input
+       (mapcat (fn [[f s]] (repeat s f)))
+       compact
        (map-indexed *)
        (reduce + 0)))
 
+(defn compact-whole-files
+  [disk]
+  (loop [to-process disk
+         processed-begin []
+         processed-end ()]
+    (cond (empty? to-process) (concat processed-begin processed-end)
+          (-> to-process peek first nil?) (recur (pop to-process)
+                                                  processed-begin
+                                                  processed-end)
+          (ffirst to-process) (recur (vec (rest to-process))
+                                     (conj processed-begin (first to-process))
+                                     processed-end)
+          :else
+          (let [[_ free-size] (first to-process)
+                [idx size] (peek to-process)
+                size-diff (- free-size size)]
+            (cond (neg? size-diff) (recur (pop to-process)
+                                          processed-begin
+                                          (conj processed-end (peek to-process)))
+                  (zero? size-diff) (recur (vec (rest (pop to-process)))
+                                           (conj processed-begin (peek to-process))
+                                           processed-end)
+                  (pos? size-diff) (recur (vec (cons [nil size-diff] (rest (pop to-process))))
+                                          (conj processed-begin (peek to-process))
+                                          processed-end))))))
+
 (defn part2
   [input]
-  input)
+  (->> input
+       compact-whole-files))
 
 (lib/check
   [part1 sample] 1928
-  [part1 puzzle] 6323641412437
-  #_#_[part2 sample] 0
+  #_#_[part1 puzzle] 6323641412437
+  [part2 sample] 2858
   #_#_[part2 puzzle] 0)
