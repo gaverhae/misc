2f5603d63fedd80323f40da2ae1a03b696a3a5fc
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-10 07:42:43 +0100

    aoc/2024/10: part1

--- advent_of_code/2024/src/t/day10.clj
+++ advent_of_code/2024/src/t/day10.clj
@@ -7,18 +7,41 @@
 
 (defn parse
   [lines]
-  lines)
+  {:grid (->> (for [y (range (count lines))
+                    x (range (count (get lines y)))]
+                [[y x] (->long (str (get-in lines [y x])))])
+              (into {}))
+   :width (count (first lines))
+   :height (count lines)})
+
+(defn neighbours
+  [[y x]]
+  [[(inc y) x] [(dec y) x] [y (inc x)] [y (dec x)]])
 
 (defn part1
-  [input]
-  input)
+  [{:keys [grid width height]}]
+  (->> (for [y0 (range height)
+             x0 (range width)
+             :when (zero? (get grid [y0 x0]))]
+         (loop [ps #{[y0 x0]}
+                v 0]
+           (if (= 9 v)
+             (count ps)
+             (recur (->> ps
+                         (mapcat neighbours)
+                         (filter (fn [p] (get grid p)))
+                         (filter (fn [p] (= (inc v) (get grid p))))
+                         set)
+                    (inc v)))))
+       (reduce + 0)))
+
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample] 36
+  [part1 puzzle] 754
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
