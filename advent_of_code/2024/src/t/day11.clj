ab33d93992fd07ce53d7bf48b3db8eae0a7cc700
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-11 11:46:44 +0100

    aoc/2024/11: part 2

--- advent_of_code/2024/src/t/day11.clj
+++ advent_of_code/2024/src/t/day11.clj
@@ -1,4 +1,4 @@
-(ns t.day11
+(ns ^:test-refresh/focus t.day11
   (:require [clojure.core.match :refer [match]]
             [clojure.set :as set]
             [clojure.string :as string]
@@ -29,14 +29,31 @@
        first
        count))
 
+(defn memo-walk
+  [memo stone n]
+  (cond (zero? n) [memo 1]
+        (contains? memo [stone n]) [memo (get memo [stone n])]
+        :else
+        (let [[memo c] (reduce (fn [[memo tot] s]
+                                 (let [[memo c] (memo-walk memo s (dec n))]
+                                   [memo (+ c tot)]))
+                               [memo 0]
+                               (step [stone]))]
+          [(assoc memo [stone n] c) c])))
+
 (defn part2
-  [input]
-  input)
+  [stones n]
+  (->> stones
+       (reduce (fn [[m tot] stone]
+                 (let [[m c] (memo-walk m stone n)]
+                   [m (+ c tot)]))
+               [{} 0])
+       second))
 
 (lib/check
   [part1 sample 1] 7
   [part1 sample1 6] 22
   [part1 sample1 25] 55312
   [part1 puzzle 25] 217812
-  #_#_[part2 sample] 0
-  #_#_[part2 puzzle] 0)
+  [part2 puzzle 25] 217812
+  [part2 puzzle 75] 259112729857522)
