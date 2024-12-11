10a65f9cf8918840a1c39e409cc702765c459597
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-11 11:12:40 +0100

    aoc/2024/11: part 1

--- advent_of_code/2024/src/t/day11.clj
+++ advent_of_code/2024/src/t/day11.clj
@@ -7,18 +7,36 @@
 
 (defn parse
   [lines]
-  lines)
+  (-> lines
+      first
+      (string/split #" ")
+      (->> (map ->long))))
+
+(defn step
+  [stones]
+  (->> stones
+       (mapcat (fn [s]
+                 (let [c (count (str s))]
+                   (cond (zero? s) [1]
+                         (even? c) [(->long (subs (str s) 0 (/ c 2)))
+                                    (->long (subs (str s) (/ c 2) c))]
+                         :else [(* 2024 s)]))))))
 
 (defn part1
-  [input]
-  input)
+  [stones n]
+  (->> (iterate step stones)
+       (drop n)
+       first
+       count))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample 1] 7
+  [part1 sample1 6] 22
+  [part1 sample1 25] 55312
+  [part1 puzzle 25] 217812
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
