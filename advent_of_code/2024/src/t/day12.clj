3f69357a4bd94fb2081aee290e714513c3235a0e
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-12 11:25:02 +0100

    aoc/2024/12: part 2 wrong (too low)

--- advent_of_code/2024/src/t/day12.clj
+++ advent_of_code/2024/src/t/day12.clj
@@ -53,16 +53,82 @@
             to-process (set (rest to-process))
             [used fence-cost] (calculate-fence-cost grid p)]
         (recur (set/difference to-process used)
-               (+ total-so-far fence-cost))))))
+               (+ total-so-far (long fence-cost)))))))
+
+(defn find-area
+  [grid p]
+  (let [plant (get grid p)]
+    (loop [to-process #{p}
+           seen #{}
+           area #{}]
+      (if (empty? to-process)
+        area
+        (let [[p & to-process] to-process]
+          (recur (set (concat to-process
+                              (->> (neighbours p)
+                                   (filter (fn [p] (= plant (get grid p))))
+                                   (remove seen)
+                                   (filter grid))))
+                 (conj seen p)
+                 (if (= plant (get grid p))
+                   (conj area p)
+                   area)))))))
+
+(defn find-fences
+  [area]
+  (->> area
+       (mapcat (fn [[y0 x0 :as p]]
+                 (->> (neighbours p)
+                      (remove area)
+                      (map (fn [[y x]]
+                             (cond (= x0 x) [:hori (/ (+ y0 y) 2) x (inc x)]
+                                   (= y0 y) [:vert (/ (+ x0 x) 2) y (inc y)]))))))))
+
+(defn count-sides
+  [area]
+  (loop [to-process (find-fences area)
+         full-sides []]
+    (if (empty? to-process)
+      (count full-sides)
+      (let [[[dir1 dim1 from1 to1 :as p1] & to-process] to-process]
+        (if-let [[_ _ from2 to2 :as p2] (->> to-process
+                                             (filter (fn [[dir2 dim2 from2 to2]]
+                                                       (and (= dir1 dir2)
+                                                            (= dim1 dim2)
+                                                            (or (= to2 from1)
+                                                                (= to1 from2)))))
+                                             first)]
+          (recur (-> to-process
+                     set
+                     (disj p2)
+                     (conj [dir1 dim1 (min from1 from2) (max to1 to2)]))
+                 full-sides)
+          (recur to-process
+                 (conj full-sides p1)))))))
+
+(defn calculate-discounted-fence-cost
+  [grid p]
+  (let [area (find-area grid p)]
+    [area (* (count area) (count-sides area))]))
 
 (defn part2
-  [input]
-  input)
+  [grid]
+  (loop [to-process (set (keys grid))
+         total-so-far 0]
+    (if (empty? to-process)
+      total-so-far
+      (let [p (first to-process)
+            to-process (set (rest to-process))
+            [used fence-cost] (calculate-discounted-fence-cost grid p)]
+        (recur (set/difference to-process used)
+               (+ total-so-far (long fence-cost)))))))
 
 (lib/check
   [part1 sample] 140
   [part1 sample1] 772
   [part1 sample2] 1930
-  [part1 puzzle] 0
-  #_#_[part2 sample] 0
-  #_#_[part2 puzzle] 0)
+  [part1 puzzle] 1522850
+  [part2 sample] 80
+  [part2 sample1] 436
+  [part2 sample2] 1206
+  [part2 puzzle] 0)
