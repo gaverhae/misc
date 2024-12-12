6b84bd019b821cbe3ce6a550e26c24fda1c1f171
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-12 11:34:28 +0100

    aoc/2024/12: rewrite part1 using part2 functions

--- advent_of_code/2024/src/t/day12.clj
+++ advent_of_code/2024/src/t/day12.clj
@@ -16,45 +16,6 @@
   [[y x]]
   [[(inc y) x] [(dec y) x] [y (inc x)] [y (dec x)]])
 
-(defn calculate-fence-cost
-  [grid p]
-  (let [plant (get grid p)]
-    (loop [perimeter 0
-           area 0
-           to-process #{p}
-           used #{}
-           seen #{}]
-      (if (empty? to-process)
-        [used (* perimeter area)]
-        (let [[p & to-process] to-process]
-          (if (= plant (get grid p))
-              (recur (+ perimeter (->> (neighbours p)
-                                       (remove (fn [p]
-                                                 (= (get grid p) plant)))
-                                       count))
-                     (inc area)
-                     (set/union (set to-process)
-                                (->> (neighbours p)
-                                     (remove seen)
-                                     (filter (fn [p]
-                                               (get grid p)))
-                                     set))
-                     (conj used p)
-                     (conj seen p))
-            (recur perimeter area to-process used (conj seen p))))))))
-
-(defn part1
-  [grid]
-  (loop [to-process (set (keys grid))
-         total-so-far 0]
-    (if (empty? to-process)
-      total-so-far
-      (let [p (first to-process)
-            to-process (set (rest to-process))
-            [used fence-cost] (calculate-fence-cost grid p)]
-        (recur (set/difference to-process used)
-               (+ total-so-far (long fence-cost)))))))
-
 (defn find-area
   [grid p]
   (let [plant (get grid p)]
@@ -83,6 +44,19 @@
                       (map (fn [[y x]]
                              (cond (= x0 x) [[:hori y0 y] x (inc x)]
                                    (= y0 y) [[:vert x0 x] y (inc y)]))))))))
+
+(defn part1
+  [grid]
+  (loop [to-process (set (keys grid))
+         total-so-far 0]
+    (if (empty? to-process)
+      total-so-far
+      (let [p (first to-process)
+            area (find-area grid p)
+            fences (find-fences area)]
+        (recur (set/difference to-process area)
+               (+ total-so-far
+                  (* (count area) (count fences))))))))
 
 (defn count-sides
   [area]
@@ -132,4 +106,4 @@
   [part2 sample2] 1206
   [part2 sample3] 236
   [part2 sample4] 368
-  [part2 puzzle] 0)
+  [part2 puzzle] 953738)
