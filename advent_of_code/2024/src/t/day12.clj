65bf6ef9295dedcb2917a195aa903cacc1f30653
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-12 10:38:56 +0100

    aoc/2024/12: part 1

--- advent_of_code/2024/src/t/day12.clj
+++ advent_of_code/2024/src/t/day12.clj
@@ -7,18 +7,62 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> (for [j (range (count lines))
+             i (range (count (get lines j)))]
+         [[j i] (get-in lines [j i])])
+       (into {})))
+
+(defn neighbours
+  [[y x]]
+  [[(inc y) x] [(dec y) x] [y (inc x)] [y (dec x)]])
+
+(defn calculate-fence-cost
+  [grid p]
+  (let [plant (get grid p)]
+    (loop [perimeter 0
+           area 0
+           to-process #{p}
+           used #{}
+           seen #{}]
+      (if (empty? to-process)
+        [used (* perimeter area)]
+        (let [[p & to-process] to-process]
+          (if (= plant (get grid p))
+              (recur (+ perimeter (->> (neighbours p)
+                                       (remove (fn [p]
+                                                 (= (get grid p) plant)))
+                                       count))
+                     (inc area)
+                     (set/union (set to-process)
+                                (->> (neighbours p)
+                                     (remove seen)
+                                     (filter (fn [p]
+                                               (get grid p)))
+                                     set))
+                     (conj used p)
+                     (conj seen p))
+            (recur perimeter area to-process used (conj seen p))))))))
 
 (defn part1
-  [input]
-  input)
+  [grid]
+  (loop [to-process (set (keys grid))
+         total-so-far 0]
+    (if (empty? to-process)
+      total-so-far
+      (let [p (first to-process)
+            to-process (set (rest to-process))
+            [used fence-cost] (calculate-fence-cost grid p)]
+        (recur (set/difference to-process used)
+               (+ total-so-far fence-cost))))))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample] 140
+  [part1 sample1] 772
+  [part1 sample2] 1930
+  [part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
