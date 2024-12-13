6e6a498cce294b91e0075ad678e0a2c9a4de1f4a
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-13 07:16:19 +0100

    aoc/2024/13: clean-up debug; way too slow

--- advent_of_code/2024/src/t/day13.clj
+++ advent_of_code/2024/src/t/day13.clj
@@ -37,40 +37,22 @@
 (defn part2
   [input offset]
   (->> input
-       (filter (fn [[ax ay bx by px py]]
-                 (->> (for [a (range 101)
-                            b (range 101)
-                            :when (and (= px (+ (* a ax) (* b bx)))
-                                       (= py (+ (* a ay) (* b by))))]
-                        (+ (* 3 a) b))
-                      sort
-                      first)))
        (map (fn [[ax ay bx by px py]]
-              (let [solution (->> (for [a (range 101)
-                                        b (range 101)
-                                        :when (and (= px (+ (* a ax) (* b bx)))
-                                                   (= py (+ (* a ay) (* b by))))]
-                                    [a b])
-                                  (sort-by (fn [[a b]] (+ (* 3 a) b)))
-                                  first)]
-                [ax ay bx by px py solution])))
-       (keep (fn [[ax ay bx by px py solution]]
+              [ax ay bx by (+ px offset) (+ py offset)]))
+       (keep (fn [[ax ay bx by px py]]
                ;; start with "all a", one coord <= and one coord >
-               (let [[a b] (loop [a (max (quot px ax) (quot py ay))
-                                  b 0]
-                             (let [cx (+ (* a ax) (* b bx))
-                                   cy (+ (* a ay) (* b by))]
-                               #_(prn [[ax ay bx by px py] a b cx cy])
-                               (cond ;; not sur I understand this case fully, but debug showed this case to happen
-                                     (and (= cx (+ px ax)) (= cy (+ py ay))) (recur (dec a) b)
-                                     (and (= px cx) (= py cy)) [a b] #_(+ (* 3 a) b)
-                                      (or (zero? a)
-                                         (and (> cx px) (> cy py))) [a b] #_nil ;; impossible
-                                     (and (<= cx px) (<= cy py)) (recur a (inc b))
-                                     (or (> cx px) (> cy py)) (recur (dec a) b)
-                                     :else (throw (ex-info "unhandled case" [ax ay bx by px py a b cx cy])))))]
-                 (when (not= [a b] solution)
-                   (prn [ax ay bx by px py solution [a b] (+ (* a ax) (* b bx)) (+ (* a ay) (* b by))])))))
+               (loop [a (max (quot px ax) (quot py ay))
+                      b 0]
+                 (let [cx (+ (* a ax) (* b bx))
+                       cy (+ (* a ay) (* b by))]
+                   (cond ;; not sure I understand this case fully, but debug showed this case to happen
+                         (and (= cx (+ px ax)) (= cy (+ py ay))) (recur (dec a) b)
+                         (and (= px cx) (= py cy)) (+ (* 3 a) b)
+                         (or (zero? a)
+                             (and (> cx px) (> cy py))) nil ;; impossible
+                         (and (<= cx px) (<= cy py)) (recur a (inc b))
+                         (or (> cx px) (> cy py)) (recur (dec a) b)
+                         :else (throw (ex-info "unhandled case" [ax ay bx by px py a b cx cy])))))))
        (reduce + 0)))
 
 (lib/check
@@ -78,4 +60,4 @@
   [part1 puzzle] 34393
   [part2 sample 0] 480
   [part2 puzzle 0] 34393
-  #_#_[part2 puzzle] 0)
+  [part2 puzzle 10000000000000] 0)
