312b5c022bcf8d9f089ba15db6f4ca615d90e1fd
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-14 11:22:56 +0100

    aoc/2024/14: part 1

--- advent_of_code/2024/src/t/day14.clj
+++ advent_of_code/2024/src/t/day14.clj
@@ -13,9 +13,37 @@
        (map (fn [[_ & p]] (map ->long p)))
        (mapv (fn [[x y dx dy]] [[y x] [dy dx]]))))
 
+(defn step
+  [width height]
+  (fn [robot]
+    (let [[[y x] [dy dx]] robot]
+      [[(mod (+ y dy) height)
+        (mod (+ x dx) width)]
+       [dy dx]])))
+
+(defn by-quadrants
+  [width height]
+  (let [y-split (quot height 2)
+        x-split (quot width 2)
+        find-q (fn [s p]
+                 (cond (< p s) 0
+                       (= p s) nil
+                       (> p s) 1))]
+    (fn [robot]
+      (let [[[y x] _] robot]
+        (when-let [h (find-q x x-split)]
+          (when-let [v (find-q y y-split)]
+            [v h]))))))
+
 (defn part1
   [robots width height]
-  robots)
+  (->> robots
+       (map (fn [r] (-> (iterate (step width height) r)
+                        (nth 100))))
+       (keep (by-quadrants width height))
+       frequencies
+       vals
+       (reduce * 1)))
 
 (defn part2
   [input]
@@ -23,6 +51,6 @@
 
 (lib/check
   [part1 sample 11 7] 12
-  #_#_[part1 puzzle 101 103] 0
+  [part1 puzzle 101 103] 236628054
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
