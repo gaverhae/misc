053555ac1b2803e93ea7c9c04f45ce559e4b8737
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-15 08:30:43 +0100

    aoc/2024/15: part 2: breaking a box, apparently

--- advent_of_code/2024/src/t/day15.clj
+++ advent_of_code/2024/src/t/day15.clj
@@ -65,6 +65,50 @@
       (let [[m & moves] moves]
         (recur (move :robot robot grid m) moves)))))
 
+(defn move-box
+  [[y0 x0 :as left] grid [dy dx :as dir]]
+  (let [right [y0 (inc x0)]
+        new-left [(+ y0 dy) (+ x0 dx)]
+        new-right [(+ y0 dy) (+ x0 dx 1)]
+        move [true (-> grid
+                       (assoc left :empty)
+                       (assoc right :empty)
+                       (assoc new-left :left)
+                       (assoc new-right :right))]
+        no-move [false grid]]
+    (match [dir (get grid new-left) (get grid new-right)]
+      [_ :wall _] no-move
+      [_ _ :wall] no-move
+      [_ :empty :empty] move
+      [[0 -1] :empty _] move
+      [[0 -1] :right _] (let [[moved? grid] (move-box [y0 (- x0 2)] grid [0 -1])]
+                          (if moved? move no-move))
+      [[0 1] _ :empty] move
+      [[0 1] _ :left] (let [[moved? grid] (move-box [y0 (+ x0 2)] grid [0 1])]
+                        (if moved? move no-move))
+      [[_ 0] :left :right] (let [[moved? grid] (move-box new-left grid dir)]
+                             (if moved? move no-move))
+      [[_ 0] :empty :left] (let [[moved? grid] (move-box new-right grid dir)]
+                             (if moved? move no-move))
+      [[_ 0] :right :empty] (let [[moved? grid] (move-box [(+ y0 dy) (dec x0)] grid dir)]
+                              (if moved? move no-move))
+      [[_ 0] :right :left] (let [[moved-right? grid] (move-box new-right grid dir)
+                                 [moved-left? grid] (move-box [(+ y0 dy) (dec x0)] grid dir)]
+                             (if (and moved-left? moved-right?) move no-move)))))
+
+(defn move-robot
+  [[y0 x0 :as pos] grid [dy dx :as dir]]
+  (let [new-pos [(+ dy y0) (+ dx x0)]
+        move [new-pos (-> grid (assoc pos :empty) (assoc new-pos :robot))]
+        no-move [pos grid]]
+    (case (get grid new-pos)
+      :empty [new-pos (-> grid (assoc pos :empty) (assoc new-pos :robot))]
+      :wall [pos grid]
+      :left (let [[moved? grid] (move-box new-pos grid dir)]
+              (if moved?  move no-move))
+      :right (let [[moved? grid] (move-box [(+ y0 dy) (+ x0 dx -1)] grid dir)]
+               (if moved? move no-move)))))
+
 (defn part2
   [{:keys [grid moves]}]
   (let [grid (->> grid
@@ -77,8 +121,15 @@
         robot (->> grid
                    (filter (fn [[[y x] c]] (= c :robot)))
                    (ffirst))]
-    [grid robot]
-    ))
+    (loop [[robot grid] [robot grid]
+           moves moves]
+      (if (empty? moves)
+        (->> grid
+             (filter (fn [[pos x]] (= x :left)))
+             (map (fn [[[y x] _]] (+ (* 100 y) x)))
+             (reduce + 0))
+        (let [[m & moves] moves]
+          (recur (move-robot robot grid m) moves))))))
 
 (lib/check
   [part1 sample] 10092
