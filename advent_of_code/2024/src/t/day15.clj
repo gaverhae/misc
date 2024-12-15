af355de767e82fbaff48840e1e21b6a105b0200b
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-15 07:57:16 +0100

    aoc/2024/15: part 1

--- advent_of_code/2024/src/t/day15.clj
+++ advent_of_code/2024/src/t/day15.clj
@@ -30,7 +30,7 @@
                 (map-indexed vector)
                 (mapcat (fn [[y [_ & line]]]
                           (->> line
-                               (map-indexed (fn [x [c]] [[y x] (if (= :robot c) :empty c)])))))
+                               (map-indexed (fn [x [c]] [[y x] c])))))
                 (into {}))
      :robot (->> grid
                  (map-indexed vector)
@@ -40,11 +40,30 @@
                                                 (when (= :robot c)
                                                   [y x]))))))
                  first)
-     :moves (->> moves (map first))}))
+     :moves (->> moves (map first) (map {:left [0 -1], :right [0 1], :down [1 0], :up [-1 0]}))}))
+
+(defn move
+  [bor [y0 x0 :as pos] grid [dy dx :as dir]]
+  (let [new-pos [(+ dy y0)(+ dx x0)]]
+    (case (get grid new-pos)
+      :empty [new-pos (-> grid (assoc pos :empty) (assoc new-pos bor))]
+      :wall [pos grid]
+      :box (let [[new-box-pos grid] (move :box new-pos grid dir)]
+             (if (= new-box-pos new-pos)
+               [pos grid]
+               [new-pos (-> grid (assoc pos :empty) (assoc new-pos bor))])))))
 
 (defn part1
-  [input]
-  input)
+  [{:keys [grid robot moves]}]
+  (loop [[robot grid] [robot grid]
+         moves moves]
+    (if (empty? moves)
+      (->> grid
+           (filter (fn [[pos x]] (= x :box)))
+           (map (fn [[[y x] _]] (+ (* 100 y) x)))
+           (reduce + 0))
+      (let [[m & moves] moves]
+        (recur (move :robot robot grid m) moves)))))
 
 (defn part2
   [input]
@@ -53,6 +72,6 @@
 (lib/check
   [part1 sample] 10092
   [part1 sample1] 2028
-  #_#_[part1 puzzle] 0
+  [part1 puzzle] 1526018
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
