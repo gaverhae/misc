c28b0071b747a0608609ca4d7aa554501e1eff56
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-16 07:08:52 +0100

    aoc/2024/16: part 1

--- advent_of_code/2024/src/t/day16.clj
+++ advent_of_code/2024/src/t/day16.clj
@@ -25,9 +25,29 @@
                                   first)))
              first)})
 
+(def clockwise
+  (->> [[0 1] [1 0] [0 -1] [-1 0]]
+       cycle
+       (partition 2 1)
+       (take 4)
+       (reduce #(apply assoc %1 %2) {})))
+
+(def counter-clockwise
+  (->> clockwise
+       (map (fn [[k v]] [v k]))
+       (into {})))
+
 (defn part1
-  [input]
-  input)
+  [{:keys [grid start end]}]
+  (lib/dijkstra-search
+    [start [0 1]]
+    (fn [[p _]] (= p end))
+    (fn [[[[y x] [dy dx]] cost :as arg]]
+      (->> [[[[y x] (clockwise [dy dx])] (+ 1000 cost)]
+            [[[y x] (counter-clockwise [dy dx])] (+ 1000 cost)]
+            [[[(+ dy y) (+ dx x)] [dy dx]] (+ 1 cost)]]
+           (filter (fn [[[p d] c]]
+                     (contains? #{\S \E \.} (get-in grid p))))))))
 
 (defn part2
   [input]
@@ -36,6 +56,6 @@
 (lib/check
   [part1 sample] 7036
   [part1 sample1] 11048
-  #_#_[part1 puzzle] 0
+  [part1 puzzle] 103512
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
