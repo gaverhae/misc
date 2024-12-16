bcd8c1510cd5c9b4fead4e12690301a0a561fcfa
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-16 06:51:33 +0100

    aoc/2024/16: parse

--- advent_of_code/2024/src/t/day16.clj
+++ advent_of_code/2024/src/t/day16.clj
@@ -7,7 +7,23 @@
 
 (defn parse
   [lines]
-  lines)
+  {:grid (->> lines (mapv vec))
+   :start (->> lines
+               (keep-indexed (fn [y line]
+                               (->> line
+                                    (keep-indexed (fn [x c]
+                                                    (when (= c \S)
+                                                      [y x])))
+                                    first)))
+               first)
+   :end (->> lines
+             (keep-indexed (fn [y line]
+                             (->> line
+                                  (keep-indexed (fn [x c]
+                                                  (when (= c \E)
+                                                    [y x])))
+                                  first)))
+             first)})
 
 (defn part1
   [input]
@@ -18,7 +34,8 @@
   input)
 
 (lib/check
-  [part1 sample] 0
+  [part1 sample] 7036
+  [part1 sample1] 11048
   #_#_[part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
