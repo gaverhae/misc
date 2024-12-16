3860a9017e9a5b47664e2bbb685c87a669f02231
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-16 07:33:42 +0100

    aoc/2024/16: part 2 sample, but too slow on puzzle

--- advent_of_code/2024/src/t/day16.clj
+++ advent_of_code/2024/src/t/day16.clj
@@ -49,13 +49,47 @@
            (filter (fn [[[p d] c]]
                      (contains? #{\S \E \.} (get-in grid p))))))))
 
+(defn dijkstra-all
+  [initial final? generate-moves]
+  (let [to-visit (java.util.PriorityQueue. 100 (fn [x y] (compare (first x) (first y))))]
+    (.add to-visit [0 initial])
+    (loop [visited #{}
+           final-cost nil
+           good-paths []]
+      (if (.isEmpty to-visit)
+        good-paths
+        (let [[cost state] (.poll to-visit)]
+          (cond
+            (and (final? state) (or (nil? final-cost) (= cost final-cost)))
+            (recur (conj visited state) cost (conj good-paths state))
+            (and final-cost (> cost final-cost))
+            (recur (conj visited state) final-cost good-paths)
+            :else
+            (do (when (not (visited state))
+                  (doseq [[nxt-state nxt-cost] (generate-moves [state cost])]
+                    (when (not (visited nxt-state))
+                      (.add to-visit [nxt-cost nxt-state]))))
+                (recur (conj visited state) final-cost good-paths))))))))
+
 (defn part2
-  [input]
-  input)
+  [{:keys [grid start end]}]
+  (->> (dijkstra-all
+         [[start] [0 1]]
+         (fn [[[p] _]] (= p end))
+         (fn [[[[[y x] :as hist] [dy dx]] cost :as arg]]
+           (->> [[[hist (clockwise [dy dx])] (+ 1000 cost)]
+                 [[hist (counter-clockwise [dy dx])] (+ 1000 cost)]
+                 [[(cons [(+ dy y) (+ dx x)] hist) [dy dx]] (+ 1 cost)]]
+                (filter (fn [[[[p] d] c]]
+                          (contains? #{\S \E \.} (get-in grid p)))))))
+       (mapcat (fn [[hist final-dir]] hist))
+       set
+       count))
 
 (lib/check
   [part1 sample] 7036
   [part1 sample1] 11048
   [part1 puzzle] 103512
-  #_#_[part2 sample] 0
-  #_#_[part2 puzzle] 0)
+  [part2 sample] 45
+  [part2 sample1] 64
+  [part2 puzzle] 0)
