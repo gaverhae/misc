54068889a0d268581652158ac21d769d6e4643ae
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-17 12:17:38 +0100

    aoc/2024/17: step

--- advent_of_code/2024/src/t/day17.clj
+++ advent_of_code/2024/src/t/day17.clj
@@ -12,11 +12,36 @@
         reg (fn [s] (let [[_ v] (re-matches #"Register .: (\d+)" s)]
                       (parse-long v)))
         prog (fn [s] (let [d (re-seq #"\d" s)]
-                       (map parse-long d)))]
+                       (mapv parse-long d)))]
     {:a (reg a)
      :b (reg b)
      :c (reg c)
-     :p (prog p)}))
+     :p (prog p)
+     :i 0
+     :out []}))
+
+(defn combo
+  [arg a b c]
+  (cond (<= 0 arg 3) arg
+        (= 4 arg) a
+        (= 5 arg) b
+        (= 6 arg) c))
+
+(defn step
+  [{:keys [a b c p i out] :as m}]
+  (let [op (get p i)
+        arg (get p (inc i))
+        m (update m :i + 2)]
+    (when (and op arg)
+      (case op
+        0 (assoc m :a (quot a (long (Math/pow 2 (combo arg a b c)))))
+        1 (assoc m :b (bit-xor b arg))
+        2 (assoc m :b (mod (combo arg a b c) 8))
+        3 (if (zero? a) m (assoc m :i arg))
+        4 (assoc m :b (bit-xor b c))
+        5 (update m :out conj (mod (combo arg) 8))
+        6 (assoc m :b (quot a (long (Math/pow 2 (combo arg a b c)))))
+        7 (assoc m :c (quot a (long (Math/pow 2 (combo arg a b c)))))))))
 
 (defn part1
   [input]
@@ -27,7 +52,7 @@
   input)
 
 (lib/check
-  [part1 sample] 0
+  [part1 sample] "4,6,3,5,6,3,5,2,1,0"
   #_#_[part1 puzzle] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
