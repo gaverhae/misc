f211642a37fdc0311c49ae4890f9c836c8e69726
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-18 09:40:07 +0100

    aoc/2024/18: part 1

--- advent_of_code/2024/src/t/day18.clj
+++ advent_of_code/2024/src/t/day18.clj
@@ -7,18 +7,33 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> lines
+       (map (fn [l] (-> l (string/split #",") (->> (map parse-long)))))))
+
+(defn generate-moves
+  [grid size]
+  (fn [[[x y] cost]]
+    (->> [[(inc x) y] [(dec x) y] [x (inc y)] [x (dec y)]]
+         (filter (fn [[x y]] (and (<= 0 x (dec size)) (<= 0 y (dec size)))))
+         (filter (fn [[x y]] (not= :wall (grid [x y]))))
+         (map (fn [p] [p (inc cost)])))))
 
 (defn part1
-  [input]
-  input)
+  [input size num-bytes]
+  (let [grid (->> input
+                  (take num-bytes)
+                  (reduce (fn [acc el] (assoc acc (vec el) :wall)) {}))]
+    (lib/dijkstra-search
+      [0 0]
+      (fn [[x y]] (= (dec size) x y))
+      (generate-moves grid size))))
 
 (defn part2
   [input]
   input)
 
 (lib/check
-  [part1 sample] 0
-  #_#_[part1 puzzle] 0
+  [part1 sample 7 12] 22
+  [part1 puzzle 71 1024] 316
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
