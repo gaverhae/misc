63589728f37102a661729f39145cd01bcf2b4858
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-20 07:53:03 +0100

    aoc/2024/20: part1 sample; very slow (turns out we can only pass 1 wall)

--- advent_of_code/2024/src/t/day20.clj
+++ advent_of_code/2024/src/t/day20.clj
@@ -19,17 +19,13 @@
             (when (not cheated?)
               (->> dirs
                    (map (fn [[dy dx]]
-                          [(+ y dy) (+ x dx)]))
-                   (mapcat (fn [[y x]]
-                             (->> dirs
-                                  (map (fn [[dy dx]]
-                                         [[(+ y dy) (+ x dx)] [y x]])))))
+                          [[(+ y dy) (+ x dx)] [y x]]))
                    (mapcat (fn [[[y x] c1]]
                              (->> dirs
                                   (map (fn [[dy dx]]
                                          [[(+ y dy) (+ x dx)] [c1 [y x]]])))))
                    (filter (fn [[p c]] (valid-pos? p)))
-                   (map (fn [[p c]] [p c (+ 3 cost)])))))))
+                   (map (fn [[p c]] [p c (+ 2 cost)])))))))
 
 (defn all-paths
   [{:keys [valid-pos? start end]}]
@@ -56,7 +52,8 @@
   (let [cost-per-cheat (all-paths input)
         cost-with-no-cheat (cost-per-cheat false)]
     (->> cost-per-cheat
-         (filter (fn [[cheat c]] (>= (- cost-with-no-cheat c) saves-at-least))))))
+         (filter (fn [[cheat c]] (>= (- cost-with-no-cheat c) saves-at-least)))
+         count)))
 
 
 (defn part2
@@ -65,16 +62,16 @@
 
 (lib/check
   [part1 sample 2] 44
-  #_#_[part1 sample 4] 30
-  #_#_[part1 sample 6] 16
-  #_#_[part1 sample 8] 14
-  #_#_[part1 sample 10] 10
-  #_#_[part1 sample 12] 8
-  #_#_[part1 sample 20] 5
-  #_#_[part1 sample 36] 4
-  #_#_[part1 sample 38] 3
-  #_#_[part1 sample 40] 2
-  #_#_[part1 sample 64] 1
-  #_#_[part1 puzzle] 0
+  [part1 sample 4] 30
+  [part1 sample 6] 16
+  [part1 sample 8] 14
+  [part1 sample 10] 10
+  [part1 sample 12] 8
+  [part1 sample 20] 5
+  [part1 sample 36] 4
+  [part1 sample 38] 3
+  [part1 sample 40] 2
+  [part1 sample 64] 1
+  [part1 puzzle 100] 0
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
