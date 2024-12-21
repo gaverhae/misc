c1e5c45989eac64a17674ec1191a524fdc9df488
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-21 11:15:52 +0100

    aoc/2024/21: prep

---
+++ advent_of_code/2024/src/t/day21.clj
@@ -0,0 +1,24 @@
+(ns ^:test-refresh/focus t.day21
+  (:require [clojure.core.match :refer [match]]
+            [clojure.set :as set]
+            [clojure.string :as string]
+            [instaparse.core :refer [parser]]
+            [t.lib :as lib :refer [->long]]))
+
+(defn parse
+  [lines]
+  lines)
+
+(defn part1
+  [input]
+  input)
+
+(defn part2
+  [input]
+  input)
+
+(lib/check
+  [part1 sample] 0
+  #_#_[part1 puzzle] 0
+  #_#_[part2 sample] 0
+  #_#_[part2 puzzle] 0)
