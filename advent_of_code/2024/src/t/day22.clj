095d613d7ddbd0b2683e66594d218a7403bcebca
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-22 11:19:43 +0100

    aoc/2024/22: single thread (~6s)
    The speed bump is mostly due to the removal of all-seq.

--- advent_of_code/2024/src/t/day22.clj
+++ advent_of_code/2024/src/t/day22.clj
@@ -29,31 +29,28 @@
 
 (defn part2
   [input]
-  (let [prices (->> input
-                    (map (fn [x]
-                           (->> (iterate next-random x)
-                                (take 2001)
-                                (map (fn [r] (mod r 10)))
-                                (partition 2 1)
-                                (map (fn [[prev cur]] [cur (- cur prev)]))
-                                (partition 4 1)
-                                (map (fn [[[_ p1] [_ p2] [_ p3] [cur p4]]]
-                                       [cur [p1 p2 p3 p4]]))))))
-        all-seqs (->> prices (mapcat (fn [vendor] (map (fn [[cur s]] s) vendor))) set)
-        by-seq (->> prices
-                    (map (fn [vendor]
-                           (->> vendor
-                                (reduce (fn [acc [p s]]
-                                          (if (contains? acc s)
-                                            acc
-                                            (assoc acc s p)))
-                                        {})))))]
-    (->> by-seq
-         (reduce (fn [acc el]
-                   (merge-with + acc el)))
-         (sort-by (fn [[k v]] (- v)))
-         first
-         second)))
+  (->> input
+       (map (fn [x]
+              (->> (iterate next-random x)
+                   (take 2001)
+                   (map (fn [r] (mod r 10)))
+                   (partition 2 1)
+                   (map (fn [[prev cur]] [cur (- cur prev)]))
+                   (partition 4 1)
+                   (map (fn [[[_ p1] [_ p2] [_ p3] [cur p4]]]
+                          [cur [p1 p2 p3 p4]])))))
+       (map (fn [vendor]
+              (->> vendor
+                   (reduce (fn [acc [p s]]
+                             (if (contains? acc s)
+                               acc
+                               (assoc acc s p)))
+                           {}))))
+       (reduce (fn [acc el]
+                 (merge-with + acc el)))
+       (sort-by (fn [[k v]] (- v)))
+       first
+       second))
 
 (lib/check
   [part1 sample] 37327623
