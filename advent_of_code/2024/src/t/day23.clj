70803c5c76c60a8526e83e1e729ec9419f67e4c9
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-23 07:25:13 +0100

    aoc/2024/23: part 1

--- advent_of_code/2024/src/t/day23.clj
+++ advent_of_code/2024/src/t/day23.clj
@@ -7,11 +7,26 @@
 
 (defn parse
   [lines]
-  lines)
+  (->> lines
+       (map (fn [s] (string/split s #"-")))
+       (mapcat (fn [[c1 c2]] [[c1 c2] [c2 c1]]))
+       (reduce (fn [acc [k v]]
+                 (update acc k (fnil conj #{}) v))
+               {})))
 
 (defn part1
   [input]
-  input)
+  (->> (for [c1 (->> input
+                       (filter (fn [[k v]] (= \t (first k))))
+                       (filter (fn [[k v]] (>= (count v) 2)))
+                       (map first))
+               c2 (get input c1)
+               c3 (get input c1)
+               :when (and (neg? (compare c2 c3))
+                          (contains? (get input c2) c3))]
+           (sort [c1 c2 c3]))
+         set
+         count))
 
 (defn part2
   [input]
@@ -19,6 +34,6 @@
 
 (lib/check
   [part1 sample] 7
-  #_#_[part1 puzzle] 0
+  [part1 puzzle] 1599
   #_#_[part2 sample] 0
   #_#_[part2 puzzle] 0)
