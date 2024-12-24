93b799d2eb37d9ad2f44ecdca36b198d6b62c7d7
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-24 06:44:01 +0100

    aoc/2024/24: compute wrong outputs

--- advent_of_code/2024/src/t/day24.clj
+++ advent_of_code/2024/src/t/day24.clj
@@ -49,7 +49,7 @@
        bits-to-num))
 
 (defn part2
-  [{:keys [wires output]} f]
+  [{:keys [wires output] :as input} f]
   (let [x (->> wires
                (filter (fn [[k v]] (= \x (first k))))
                sort
@@ -60,12 +60,26 @@
                sort
                (map (fn [[k [_ v]]] v))
                bits-to-num)
-        z-expected (num-to-bits (f x y))]
-    [x y z-expected]))
+        expected-z (->> (num-to-bits (f x y))
+                        reverse
+                        (map-indexed (fn [idx b]
+                                       [(format "z%02d" idx) b]))
+                        (into {}))
+        actual-z (->> (part1 input)
+                      num-to-bits
+                      reverse
+                      (map-indexed (fn [idx b]
+                                     [(format "z%02d" idx) b]))
+                      (into {}))
+        wrong-z (->> actual-z
+                     (remove (fn [[k v]] (= v (get expected-z k))))
+                     (into {}))]
+
+    [x y expected-z actual-z wrong-z]))
 
 (lib/check
   [part1 sample] 4
   [part1 sample1] 2024
   [part1 puzzle] 57344080719736
   [part2 sample2 bit-and] "z00,z01,z02,z05"
-  [part2 puzzle +] 0)
+  #_#_[part2 puzzle +] 0)
