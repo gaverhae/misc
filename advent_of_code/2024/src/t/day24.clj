f27146be03478cf98b601999298c53681e144759
Gary Verhaegen <gary.verhaegen@gmail.com>
2024-12-24 09:31:50 +0100

    aoc/2024/24: more insightful insights\!

--- advent_of_code/2024/src/t/day24.clj
+++ advent_of_code/2024/src/t/day24.clj
@@ -65,8 +65,20 @@
                 [:and in1 in2] (bit-and (! in1) (! in2)))))
        bits-to-num))
 
+;; bit-add is
+;; z = (bit-xor (bit-xor x y) carry)
+;;
+;; He's probably trolling us by having more complicated expressions, but the
+;; important part, I think, is that once we have a wron wiring for a given
+;; output bit, we'll probably taint any number of subsequent bits with the
+;; carry. This suggests an approach where I can check my output bits one at a
+;; time starting from the lowest one.
+;;
+;; We should double-check that no output bit depends on "higher" input bits,
+;; but that's probably too obvious.
+
 (defn part2
-  [{:keys [wires output] :as input} f]
+  [{:keys [wires output] :as input}]
   (let [input-size (->> wires keys (filter (comp #{:x :y :z} first)) (map second) (apply max))
         max-input (long (Math/pow 2 (inc input-size)))
         rand-input (fn [] [(long (rand max-input)) (long (rand max-input))])
@@ -78,6 +90,12 @@
                             [:y y] [:y y]
                             [:inner i] (! (wires w))
                             [op in1 in2] [op (! in1) (! in2)]))))
+        _ (assert (->> exprs
+                       (every? (fn ! [[n e]]
+                                 (match e
+                                   [:x x] (<= x n)
+                                   [:y y] (<= y n)
+                                   [_ a b] (and (! [n a]) (! [n b])))))))
         eval-expr (fn [e x y] false)
         valid-expr? (fn [[n expr]]
                       (->> (repeatedly 1000 rand-input)
@@ -90,4 +108,4 @@
   [part1 sample] 4
   [part1 sample1] 2024
   [part1 puzzle] 57344080719736
-  [part2 puzzle +] 0)
+  [part2 puzzle] 0)
