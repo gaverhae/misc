ac303e08334f26149a8a103dbca909ef0338e03a
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-03-14 12:49:21 +0100

    tm-compare: commit old changes
    No idea when they were made.

--- collect_data.sh
+++ collect_data.sh
@@ -14,12 +14,12 @@ BUS=(/Volumes/.timemachine/*/*/*.backup)
 tmp=$(mktemp)
 
 for i in $(seq 1 $((${#BUS[@]} - 2))); do
-    target_file=cumul/$(basename ${BUS[$i]})_$(basename ${BUS[i+1]}).txt
+    target_file=cumul/$(basename ${BUS[$i]})_$(basename ${BUS[i+1]}).txt.gz
     if [ -f "$target_file" ]; then
         echo "$target_file exists; skipping"
     else
         echo "Comparing $(basename ${BUS[$i]}) <> $(basename ${BUS[i+1]})..."
-        tmutil compare "${BUS[$i]}" "${BUS[i+1]}" > $tmp
+        tmutil compare "${BUS[$i]}" "${BUS[i+1]}" | gzip -9 > $tmp
         mv $tmp $target_file
     fi
 done
