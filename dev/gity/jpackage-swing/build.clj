46cfc9db4506c5f50fcf84f35444b459d9854ce7
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-07-03 17:01:28 +0200

    gity: something works, at last

---
+++ dev/gity/jpackage-swing/build.clj
@@ -0,0 +1,20 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(defn clean
+  [_]
+  (b/delete {:path "target"}))
+
+(defn uberjar
+  [_]
+  (let [basis (b/create-basis {:project "deps.edn"})]
+    (clean nil)
+    (b/copy-dir {:src-dirs ["src" "resources"]
+                 :target-dir "target"})
+    (b/compile-clj {:basis basis
+                    :ns-compile '[main]
+                    :class-dir "target/classes"})
+    (b/uber {:class-dir "target/classes"
+             :uber-file "target/gity.jar"
+             :basis basis
+             :main 'main})))
