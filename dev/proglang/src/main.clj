585c3f8e1ccbd4cfe4727c5fff9a1f2474c03d23
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-07-20 19:36:44 +0200

    proglang: implement function application (#399)
    This seems to sort of work, but environment management is not clear
    enough in my mind that I'd be confident this is fully working.
    
    And by "working" here I mean for the cases it covers; as of right now
    it's wholly unclear to me how to manage the commented case, which
    currently does not work. Specifically: how to handle mutations in the
    captured environment. I mean, I can see options if I'm willing to use
    mutation myself in my environments, but... I'd prefer not to do that, I
    think?

--- dev/proglang/src/main.clj
+++ dev/proglang/src/main.clj
@@ -48,24 +48,43 @@
   [env node]
   (case (first node)
     :int (let [[_ i] node]
-           [env (parse-long i)])
+           [env [:int (parse-long i)]])
     :sum (let [[_ & terms] node
                [env vs] (reduce (fn [[env vs] n]
-                                  (let [[env v] (eval-pl env n)]
-                                    [env (conj vs v)]))
-                                [env []]
-                                terms)]
-           [env (reduce + 0 vs)])
+                                   (let [[env v] (eval-pl env n)]
+                                     [env (conj vs v)]))
+                                 [env []]
+                                 terms)]
+           (assert (every? (fn [[t v]] (= t :int)) vs))
+           [env [:int (reduce + 0 (map second vs))]])
     :product (let [[_ & factors] node
                    [env vs] (reduce (fn [[env vs] n]
-                                      (let [[env v] (eval-pl env n)]
-                                        [env (conj vs v)]))
-                                    [env []]
-                                    factors)]
-               [env (reduce * 1 vs)])
+                                       (let [[env v] (eval-pl env n)]
+                                         [env (conj vs v)]))
+                                     [env []]
+                                     factors)]
+               (assert (every? (fn [[t v]] (= t :int)) vs))
+               [env [:int (reduce * 1 (map second vs))]])
     :assign (let [[_ [_ n] expr] node
                   [env v] (eval-pl env expr)]
               [(assoc env n v) nil])
+    :def (let [[_ fn-name args body] node]
+           [(assoc env fn-name [:fn args body env]) nil])
+    :app (let [[_ f & args] node
+               [env evaled-f] (eval-pl env f)
+               _ (assert (= :fn (first evaled-f)))
+               [_ params body captured-env] evaled-f
+               [env evaled-args] (reduce (fn [[env vs] n]
+                                           (let [[env v] (eval-pl env n)]
+                                             [env (conj vs v)]))
+                                         [env []]
+                                         args)]
+           [env (second (eval-pl (merge env
+                                        captured-env
+                                        (zipmap params evaled-args))
+                                 (cons :S body)))])
+    :return (let [[_ expr] node]
+              (eval-pl env expr))
     :identifier (let [[_ n] node]
                   [env (get env n)])
     :S (let [[_ & stmts] node]
@@ -90,7 +109,7 @@
 
 (defn run-file
   [file]
-  (second (eval-pl {} (parse (slurp file)))))
+  (second (second (eval-pl {} (parse (slurp file))))))
 
 (defn usage
   []
