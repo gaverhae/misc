14e662503d6bfe9516b6fabd5dcc36c16e98dc66
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-07-23 15:50:09 +0200

    gity: clean up

--- dev/gity/build.clj
+++ dev/gity/build.clj
@@ -1,51 +1,20 @@
 (ns build
-  (:require [clojure.java.process :as process]
-            [clojure.tools.build.api :as b]))
+  (:require [clojure.tools.build.api :as b]))
 
-(def uber-file "target/uber.jar")
-(def class-dir "target/classes")
-
-(defn clean [_]
+(defn clean
+  [_]
   (b/delete {:path "target"}))
 
-(defn uber [params]
-  (clean params)
+(defn uberjar
+  [_]
   (let [basis (b/create-basis {:project "deps.edn"})]
+    (clean nil)
     (b/copy-dir {:src-dirs ["src" "resources"]
-                 :target-dir class-dir})
+                 :target-dir "target"})
     (b/compile-clj {:basis basis
-                    :src-dirs ["src"]
-                    :class-dir class-dir})
-    (b/uber {:class-dir class-dir
-             :uber-file uber-file
+                    :ns-compile '[main]
+                    :class-dir "target/classes"})
+    (b/uber {:class-dir "target/classes"
+             :uber-file "target/gity.jar"
              :basis basis
              :main 'main})))
-
-(defn shell
-  [& cmd+args]
-  (let [p (apply process/start
-                 {:in :inherit
-                  :out :inherit
-                  :err :inherit}
-                 cmd+args)
-        result @(process/exit-ref p)]
-    (when (not (zero? result))
-      (throw (ex-info "Subprocess exited with non-zero value."
-                      {:code result
-                       :command cmd+args})))))
-
-(defn native [params]
-  (uber params)
-  (shell "native-image"
-         "--initialize-at-build-time"
-         "-H:+UnlockExperimentalVMOptions" "-H:-CheckToolchain"
-         "-Djava.awt.headless=false"
-         "--initialize-at-run-time=sun.awt.AWTAutoShutdown"
-         "--initialize-at-run-time=javax.swing.RepaintManager"
-         "--initialize-at-run-time=java.awt.GraphicsEnvironment$LocalGE"
-         "--initialize-at-run-time=sun.java2d.opengl.OGLSurfaceData"
-         "--initialize-at-run-time=sun.java2d.metal.MTLSurfaceData"
-         "--initialize-at-run-time= java.awt.EventDispatchThread"
-         "--trace-object-instantiation=java.lang.Thread"
-         "-jar" uber-file
-         "gity"))
