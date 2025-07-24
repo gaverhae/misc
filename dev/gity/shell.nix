e51471c0dcc7fc2d18fd9478387120642d83e814
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-07-24 13:30:15 +0200

    gity: t/package command

--- dev/gity/shell.nix
+++ dev/gity/shell.nix
@@ -4,6 +4,7 @@ in
 pkgs.mkShell {
   LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
   buildInputs = with pkgs; [
+    babashka
     bash
     cacert
     clojure
