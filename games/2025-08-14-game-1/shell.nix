0dc9181515d6502c61b0bcb16dda13dcbd843216
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-08-14 14:03:34 +0200

    g/g-1: nix, direnv, & cljs-josh (#469)

---
+++ games/2025-08-14-game-1/shell.nix
@@ -0,0 +1,13 @@
+let
+  pkgs = import ./nix/nixpkgs.nix {};
+in
+pkgs.mkShell {
+  LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
+  buildInputs = with pkgs; [
+    bash
+    cacert
+    curl
+    jq
+    nodejs
+  ];
+}
