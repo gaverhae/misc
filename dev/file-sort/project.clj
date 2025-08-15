da0a65cee58d3542cbbe7ba88a89f08de688ba35
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-08-15 12:23:35 +0200

    fs: bump Clojure version (#362)

--- dev/file-sort/project.clj
+++ dev/file-sort/project.clj
@@ -1,5 +1,5 @@
 (defproject t "app"
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.1"]
                  [org.clojure/core.async "1.8.741"]
                  [org.clojure/core.match "1.1.0"]
                  [org.jline/jline "3.30.4"]]
