0f88afaad69f3248384d119504a4d0820226f9ad
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-08-15 12:22:47 +0200

    fs: experiment with jline (#360)
    This does _not_ work as expected. What I4m trying to do is to have an
    interactive experience where the program reacts to individual key
    presses, rather than waiting for me to press enter.

--- dev/file-sort/project.clj
+++ dev/file-sort/project.clj
@@ -1,7 +1,8 @@
 (defproject t "app"
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [org.clojure/core.async "1.8.741"]
-                 [org.clojure/core.match "1.1.0"]]
+                 [org.clojure/core.match "1.1.0"]
+                 [org.jline/jline "3.30.4"]]
   :main ^:skip-aot t.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
