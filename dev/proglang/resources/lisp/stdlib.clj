4c751dcb84a638278834c402db6901593de2cd4a
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-10-24 14:57:54 +0200

    proglang/lisp: add defn to stdlib

--- dev/proglang/resources/lisp/stdlib.clj
+++ dev/proglang/resources/lisp/stdlib.clj
@@ -1,3 +1,11 @@
+(def defn
+  (macro [n args & body]
+    (list (quote def) n
+          (cons (quote fn)
+                (cons n
+                      (cons args
+                            body))))))
+
 (def not
   (fn [v]
     (if (= v false)
