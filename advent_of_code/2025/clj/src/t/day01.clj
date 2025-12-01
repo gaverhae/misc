142413e90a3b325290bdc18d7aec76098753864f
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-12-01 06:12:18 +0100

    aoc/2025/01: part1

--- advent_of_code/2025/clj/src/t/day01.clj
+++ advent_of_code/2025/clj/src/t/day01.clj
@@ -1,15 +1,46 @@
 (ns t.day01
   (:require [clojure.string :as string]
-            [t.lib :as lib]))
+            [clojure.java.io :as io]
+            #_[t.lib :as lib]))
 
 (defn parse
   [text]
-  nil)
+  (->> text
+       (string/split-lines)
+       (map (fn [line]
+              (* (case (first line)
+                   \L -1
+                   \R +1)
+                 (parse-long (subs line 1)))))))
 
 (defn part1
   [input]
-  nil)
+  (reduce (fn [[num-0 cur-pos] steps]
+            (let [new-pos (mod (+ cur-pos steps) 100)]
+              [(cond-> num-0
+                 (zero? new-pos) inc)
+               new-pos]))
+          [0 50]
+          input))
 
 (defn part2
   [input]
   nil)
+
+(comment
+  (-> (io/resource "day01_sample.txt")
+      (slurp)
+      (parse))
+  (-> (io/resource "day01_sample.txt")
+      (slurp)
+      (parse)
+      part1)
+  (-> (io/resource "day01_input.txt")
+      (slurp)
+      (parse)
+      part1)
+
+
+  )
+
+
