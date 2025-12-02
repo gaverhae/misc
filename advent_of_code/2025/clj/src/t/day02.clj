72b7ff420f2259df9b230330423543b2e4aff84f
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-12-02 08:09:32 +0100

    aoc/2025/02: part1 sample

--- advent_of_code/2025/clj/src/t/day02.clj
+++ advent_of_code/2025/clj/src/t/day02.clj
@@ -5,12 +5,23 @@
 (defn parse
   [text]
   (-> text
+      (string/split-lines)
+      first
       (string/split #",")
-      (->> (map (fn [r] (string/split r #"-")))
+      (->> (map (fn [r] (prn r)  (string/split r #"-")))
            (map (fn [[a b]] [(parse-long a) (parse-long b)])))))
 
 (defn part1
-  [input])
+  [input]
+  (->> input
+       (mapcat (fn [[a b]] (prn [a b]) (range a (inc b))))
+       (filter (fn [d]
+                 (let [s (str d)
+                       c (count s)
+                       c2 (quot c 2)]
+                   (and (zero? (mod c 2))
+                        (= (take c2 s) (drop c2 s))))))
+       (reduce + 0)))
 
 (defn part2
   [input])
@@ -21,5 +32,10 @@
       (slurp)
       (parse))
 
+  (-> (io/resource "day02-sample.txt")
+      (slurp)
+      (parse)
+      part1)
+1227775554
 
          )
