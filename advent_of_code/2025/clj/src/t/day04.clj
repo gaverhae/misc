63890e9b7a2d15fdf6e70c2007e5be325986241f
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-12-04 16:36:16 +0100

    aoc/2025/04: part 1

--- advent_of_code/2025/clj/src/t/day04.clj
+++ advent_of_code/2025/clj/src/t/day04.clj
@@ -1,5 +1,6 @@
 (ns t.day04
   (:require [clojure.string :as string]
+            [clojure.set :as set]
             [clojure.java.io :as io]))
 
 (defn parse
@@ -14,9 +15,17 @@
        (apply concat)
        set))
 
+(defn neighbours
+  [[y x]]
+  #{[(dec y) (dec x)] [(dec y) x] [(dec y) (inc x)]
+    [     y  (dec x)]             [     y  (inc x)]
+    [(inc y) (dec x)] [(inc y) x] [(inc y) (inc x)]})
+
 (defn part1
   [input]
-  )
+  (->> input
+       (filter (fn [p] (< (count (set/intersection input (neighbours p))) 4)))
+       count))
 
 (defn part2
   [input]
@@ -32,11 +41,13 @@
       (slurp)
       (parse)
       part1)
+13
 
   (-> (io/resource "day04-input.txt")
       (slurp)
       (parse)
       part1)
+1533
 
   (-> (io/resource "day04-sample.txt")
       (slurp)
