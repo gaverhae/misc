810ceb1e6283519f4c287253703966cafd06c5a7
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-12-05 09:11:30 +0100

    aoc/2025/05: prep

---
+++ advent_of_code/2025/clj/src/t/day05.clj
@@ -0,0 +1,44 @@
+(ns t.day05
+  (:require [clojure.string :as string]
+            [clojure.set :as set]
+            [clojure.java.io :as io]))
+
+(defn parse
+  [text]
+  )
+
+(defn part1
+  [input]
+  )
+
+(defn part2
+  [input]
+  )
+
+(comment
+
+  (-> (io/resource "day05-sample.txt")
+      (slurp)
+      (parse))
+
+  (-> (io/resource "day05-sample.txt")
+      (slurp)
+      (parse)
+      part1)
+
+  (-> (io/resource "day05-input.txt")
+      (slurp)
+      (parse)
+      part1)
+
+  (-> (io/resource "day05-sample.txt")
+      (slurp)
+      (parse)
+      part2)
+
+  (-> (io/resource "day05-input.txt")
+      (slurp)
+      (parse)
+      part2)
+
+         )
