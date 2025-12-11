5de51a0869ac117cf6def697cdd0d9f2d500b1ee
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-12-11 17:59:56 +0100

    aoc/2025/11: parse

--- advent_of_code/2025/clj/src/t/day11.clj
+++ advent_of_code/2025/clj/src/t/day11.clj
@@ -4,9 +4,19 @@
             [clojure.java.io :as io]
             [instaparse.core :as insta]))
 
+
 (defn parse
   [text]
-  )
+  (let [p (insta/parser "<S> = from <':'> to
+                         from = name
+                         to = (<' '> name)*
+                         <name> = #'[a-z]{3}'")]
+    (->> text
+       string/split-lines
+       (map (fn [line]
+              (let [[[_ from] [_ & to]] (p line)]
+                [from to])))
+       (into {}))))
 
 (defn part1
   [input]
@@ -21,6 +31,16 @@
   (-> (io/resource "day11-sample.txt")
       (slurp)
       (parse))
+{"eee" ("out"),
+ "ggg" ("out"),
+ "ccc" ("ddd" "eee" "fff"),
+ "you" ("bbb" "ccc"),
+ "hhh" ("ccc" "fff" "iii"),
+ "bbb" ("ddd" "eee"),
+ "ddd" ("ggg"),
+ "aaa" ("you" "hhh"),
+ "fff" ("out"),
+ "iii" ("out")}
 
   (-> (io/resource "day11-sample.txt")
       (slurp)
