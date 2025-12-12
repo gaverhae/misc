a53a510b2ae54aa1a87aa362b10fb3e6d40860df
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-12-12 08:59:35 +0100

    aoc/2025/12: parse

--- advent_of_code/2025/clj/src/t/day12.clj
+++ advent_of_code/2025/clj/src/t/day12.clj
@@ -6,7 +6,28 @@
 
 (defn parse
   [text]
-  )
+  (let [p (insta/parser "S = shapes trees
+                         shapes = shape +
+                         shape = num <':\\n'> shape-line+ <'\\n'>
+                         shape-line = ('.' | '#') + <'\\n'>
+                         trees = tree +
+                         tree = num <'x'> num <':'> (<' '> num)+ <'\\n'>
+                         num = #'\\d+'")
+        [_ [_ & shapes] [_ & trees]] (p text)]
+    {:shapes (->> shapes
+                  (mapv (fn [[_ [_ n] & lines]]
+                          {:idx (parse-long n)
+                           :lines (->> lines
+                                       (mapv (fn [[_ & cs]]
+                                               (->> cs
+                                                    (mapv {"#" true, "." false})))))})))
+
+     :trees (->> trees
+                 (map (fn [[_ [_ x] [_ y] & shapes]]
+                        {:x (parse-long x)
+                         :y (parse-long y)
+                         :shapes (->> shapes
+                                      (map (fn [[_ n]] (parse-long n))))})))}))
 
 (defn part1
   [input]
@@ -21,6 +42,15 @@
   (-> (io/resource "day12-sample.txt")
       (slurp)
       (parse))
+{:shapes [{:idx 0, :lines [[true true true] [true true false] [true true false]]}
+          {:idx 1, :lines [[true true true] [true true false] [false true true]]}
+          {:idx 2, :lines [[false true true] [true true true] [true true false]]}
+          {:idx 3, :lines [[true true false] [true true true] [true true false]]}
+          {:idx 4, :lines [[true true true] [true false false] [true true true]]}
+          {:idx 5, :lines [[true true true] [false true false] [true true true]]}]
+ :trees ({:x 4, :y 4, :shapes (0 0 0 0 2 0)}
+         {:x 12, :y 5, :shapes (1 0 1 0 2 2)}
+         {:x 12, :y 5, :shapes (1 0 1 0 3 2)})}
 
   (-> (io/resource "day12-sample.txt")
       (slurp)
