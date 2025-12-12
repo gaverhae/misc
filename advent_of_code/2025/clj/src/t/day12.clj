f97d28146a4b262babbab1f674db9665af5fe51f
Gary Verhaegen <gary.verhaegen@gmail.com>
2025-12-12 21:25:26 +0100

    aoc/2025/12: slightly different approach, not measurably faster

--- advent_of_code/2025/clj/src/t/day12.clj
+++ advent_of_code/2025/clj/src/t/day12.clj
@@ -87,7 +87,7 @@
 
 (defn works?
   [shapes]
-  (let [all-shapes (->> shapes
+  (let [all-shapes (->> (assoc shapes :cell #{[0 0]})
                         (map (fn [[idx bs]]
                                [idx (all-orientations bs)]))
                         (into {}))]
@@ -95,36 +95,34 @@
       (loop [to-try [{:free? (set (for [y (range h)
                                         x (range w)]
                                     [y x]))
-                      :placed {}
-                      :to-place (->> shapes
-                                     (mapcat (fn [[idx n]]
-                                               (repeat n idx))))}]]
+                      :to-place (assoc shapes
+                                       :cell (- (* w h)
+                                                (->> shapes
+                                                     (map (fn [[idx n]]
+                                                            (* n (count (first (get all-shapes idx))))))
+                                                     (reduce + 0))))}]]
         (if (empty? to-try)
           false
-          (let [[{:keys [free? to-place placed]} & to-try] to-try]
+          (let [[{:keys [free? to-place]} & to-try] to-try]
             (if (empty? to-place)
               true
-              (let [[t & to-place] to-place]
-                (recur (concat (->> (get all-shapes t)
-                                    (mapcat (fn [g]
-                                              (->> (if-let [p (placed g)]
-                                                     (->> free?
-                                                          sort
-                                                          (drop-while (fn [f] (pos? (compare p f)))))
-                                                     (sort free?))
-                                                   (map (fn [[y x]] [[y x] (move g y x)]))
-                                                   (filter (fn [[_ g]] (set/subset? g free?)))
-                                                   (map (fn [[p g]]
+              (let [[dy dx] (->> free? sort first)
+                    next-steps (->> to-place
+                                    keys
+                                    (mapcat (fn [k]
+                                              (->> (get all-shapes k)
+                                                   (map (fn [g] (move g dy dx)))
+                                                   (filter (fn [g] (set/subset? g free?)))
+                                                   (map (fn [g]
                                                           {:free? (set/difference free? g)
-                                                           :placed (assoc placed g p)
-                                                           :to-place to-place}))))))
-                               to-try))))))))))
+                                                           :to-place (if (= 1 (get to-place k))
+                                                                       (dissoc to-place k)
+                                                                       (update to-place k dec))}))))))]
+                (recur (concat next-steps to-try))))))))))
 
 (defn part1
   [input]
   (->> (:trees input)
-       (take 2)
-       #_(drop 2)
        (map (works? (:shapes input)))
        #_count))
 
@@ -146,12 +144,11 @@
  :trees ({:w 4, :h 4, :shapes {4 2}}
          {:w 12, :h 5, :shapes {0 1, 2 1, 4 2, 5 2}}
          {:w 12, :h 5, :shapes {0 1, 2 1, 4 3, 5 2}})}
-(defn p []
+
   (-> (io/resource "day12-sample.txt")
       (slurp)
       (parse)
       (part1))
-  )
 
   (-> (io/resource "day12-input.txt")
       (slurp)
