0730a9eea8718be59d75a62b38279e223cd39c12
Gary Verhaegen <gary.verhaegen@gmail.com>
2026-02-05 20:38:11 +0100

    bump lein plugins

--- dev/file-sort/project.clj
+++ dev/file-sort/project.clj
@@ -1,7 +1,7 @@
 (defproject t "app"
-  :dependencies [[org.clojure/clojure "1.12.1"]
+  :dependencies [[org.clojure/clojure "1.12.4"]
                  [org.clojure/core.async "1.8.741"]
-                 [org.clojure/core.match "1.1.0"]]
+                 [org.clojure/core.match "1.1.1"]]
   :main ^:skip-aot t.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
