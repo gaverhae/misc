031eb27fe75e55de6cf65f80d40e867d9028bb11
Gary Verhaegen <gary.verhaegen@gmail.com>
2026-05-27 09:08:14 +0200

    aoc/2024/06: obstacles as set (~8.2s -> ~5.4s) (#554)

--- advent_of_code/2024/src/t/day06.clj
+++ advent_of_code/2024/src/t/day06.clj
@@ -5,55 +5,54 @@
 
 (defn parse
   [lines]
-  {:map (->> lines
-             (mapv (fn [line]
-                    (mapv (fn [c] (case c
-                                    \. true
-                                    \^ true
-                                    \# false))
-                          line))))
-   :guard [(->> (for [y (range (count lines))
-                      :let [line (nth lines y)]
-                      x (range (count line))
-                      :let [c (nth line x)]
-                      :when (= c \^)]
-                  [y x])
-                first)
-           [-1 0]]})
+  (let [w (count (first lines))
+        h (count lines)]
+    (->> (for [y (range h)
+               x (range w)]
+           [y x])
+         (reduce (fn [acc p]
+                   (case (get-in lines p)
+                     \. acc
+                     \# (update acc :obstacles conj p)
+                     \^ (assoc acc :guard p)))
+                 {:width w, :height h, :obstacles #{}}))))
 
 (def turn
-  {[-1 0] [0 1]
-   [0 1] [1 0]
-   [1 0] [0 -1]
-   [0 -1] [-1 0]})
+  (->> [[-1 0] [0 1] [1 0] [0 -1]]
+       cycle (partition 2 1) (take 4) (map vec) (into {})))
+
+(defn step
+  [p d]
+  [(+ (get p 0) (get d 0))
+   (+ (get p 1) (get d 1))])
+
+(defn inside?
+  [p w h]
+  (let [y (get p 0)
+        x (get p 1)]
+    (and (<= 0 x) (< x w) (<= 0 y) (< y h))))
 
 (defn find-path
-  [{m :map, [p d] :guard}]
-  (loop [[y0 x0] p
-         [dy dx] d
+  [w h obs g]
+  (loop [p g
+         d [-1 0]
          visited? #{[p d]}]
-    (let [[y x] [(+ y0 dy) (+ x0 dx)]
-          nxt (get-in m [y x] :out)]
+    (let [nxt (step p d), v [nxt d]]
       (cond
-        (visited? [[y x] [dy dx]]) [:loop]
-        (= false nxt) (recur [y0 x0] (turn [dy dx]) visited?)
-        (= true nxt) (recur [y x] [dy dx] (conj visited? [[y x] [dy dx]]))
-        (= :out nxt) [:exit (->> visited? (map first) set)]))))
+        (visited? v) :loop
+        (contains? obs nxt) (recur p (turn d) visited?)
+        (inside? nxt w h) (recur nxt d (conj visited? v))
+        :else (into #{} (map first) visited?)))))
 
 (defn part1
-  [input]
-  (match (find-path input)
-    [:loop] :not-expected
-    [:exit cells] (count cells)))
+  [{:keys [width height obstacles guard]}]
+  (count (find-path width height obstacles guard)))
 
 (defn part2
-  [{m :map, [p d] :guard :as input}]
-  (->> (find-path input)
-       second
+  [{:keys [width height obstacles guard]}]
+  (->> (find-path width height obstacles guard)
        (filter (fn [obs]
-                 (match (find-path (update input :map assoc-in obs false))
-                   [:loop] true
-                   [:exit _] false)))
+                 (= :loop (find-path width height (conj obstacles obs) guard))))
        count))
 
 (lib/check
