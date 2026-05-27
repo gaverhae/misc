373fef75e1575e6f3b01af475146273bcb51b43e
Gary Verhaegen <gary.verhaegen@gmail.com>
2026-05-27 09:04:58 +0200

    c/wpe: week 03 (#229)

---
+++ courses/2024/weekly-python-exercise-2022-b3/src/day03.py
@@ -0,0 +1,17 @@
+import pathlib
+import tarfile
+import tempfile
+import zipfile
+
+def tar_to_zip(*files, zippath = '.'):
+    for file in files:
+        if tarfile.is_tarfile(file):
+            with tempfile.TemporaryDirectory() as extract_dir:
+                with tarfile.open(file) as tf:
+                    tf.extractall(extract_dir)
+                with zipfile.ZipFile(pathlib.Path(zippath) / file.with_suffix(".zip"), 'x') as zf:
+                    for extracted_file in pathlib.Path(extract_dir).glob('*'):
+                        zf.write(extract_dir / extracted_file)
+        else:
+            print("Couldn't read from: " + str(file))
+
