9f0cd391a7eeeeef3e352e11dfaca85ce2c1a441
Gary Verhaegen <gary.verhaegen@gmail.com>
2026-06-28 17:40:50 +0200

    aoc/2025/05 (#567)

---
+++ advent_of_code/2025/clj/src/t/day05.clj
@@ -0,0 +1,92 @@
+(ns t.day05
+  (:require [clojure.string :as string]
+            [clojure.set :as set]
+            [clojure.java.io :as io]))
+
+(defn parse
+  [text]
+  (let [fresh (->> text
+                   (string/split-lines)
+                   (take-while #(not= "" %))
+                   (map (fn [line] (let [[begin end] (string/split line #"-")]
+                                     [(parse-long begin) (parse-long end)]))))
+        available (->> text
+                       (string/split-lines)
+                       (drop-while #(not= "" %))
+                       rest
+                       (map parse-long))]
+    {:fresh fresh
+     :available available}))
+
+(defn is-fresh?
+  [fresh]
+  (fn [ingredient]
+    (loop [to-check fresh]
+      (if (empty? to-check)
+        false
+        (let [[[begin end] & to-check] to-check]
+          (if (<= begin ingredient end)
+            true
+            (recur to-check)))))))
+
+(defn part1
+  [{:keys [fresh available]}]
+  (->> available
+       (filter (is-fresh? fresh))
+       count))
+
+(defn overlap?
+  [[b1 e1 :as r1] [b2 e2 :as r2]]
+  (if (> b1 b2)
+    (overlap? r2 r1)
+    (<= b2 e1)))
+
+(defn merge-ranges
+  [[b1 e1] [b2 e2]]
+  [(min b1 b2) (max e1 e2)])
+
+(defn part2
+  [{:keys [fresh]}]
+  (->> fresh
+       sort
+       (reduce (fn [acc el]
+                 (let [last-r (peek acc)]
+                   (cond (nil? last-r) [el]
+                         (overlap? last-r el) (conj (pop acc)
+                                                    (merge-ranges last-r el))
+                         :else (conj acc el))))
+               [])
+       (map (fn [[b e]] (inc (- e b))))
+       (reduce + 0)))
+
+(comment
+
+  (-> (io/resource "day05-sample.txt")
+      (slurp)
+      (parse))
+
+  (-> (io/resource "day05-sample.txt")
+      (slurp)
+      (parse)
+      part1)
+3
+
+  (-> (io/resource "day05-input.txt")
+      (slurp)
+      (parse)
+      part1)
+558
+
+  (-> (io/resource "day05-sample.txt")
+      (slurp)
+      (parse)
+      part2)
+14
+
+  (-> (io/resource "day05-input.txt")
+      (slurp)
+      (parse)
+      part2)
+344813017450467
+
+         )
